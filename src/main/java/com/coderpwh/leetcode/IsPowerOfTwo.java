9fd4cb152a8a3d8ee6476f897eb4bc0cacfe2053
coderpwh <pwhfbc@outlook.com>
2020-12-28 10:17:19 +0800

    Update IsPowerOfTwo.java
    update leetcode

--- src/main/java/com/coderpwh/leetcode/IsPowerOfTwo.java
+++ src/main/java/com/coderpwh/leetcode/IsPowerOfTwo.java
@@ -1,6 +1,29 @@
+
 package com.coderpwh.leetcode;
 
 /**
+ *   231 2的幂
+ *
+ *  给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ *
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: true
+ * 解释: 20 = 1
+ * 示例 2:
+ *
+ * 输入: 16
+ * 输出: true
+ * 解释: 24 = 16
+ * 示例 3:
+ *
+ * 输入: 218
+ * 输出: false
+ *
+ *
+ *
+ *
  * @author coderpwh
  */
 public class IsPowerOfTwo {
@@ -15,6 +38,16 @@ public class IsPowerOfTwo {
     }
 
 
+    /****
+     *  1. 时间复杂度为 O(longn)
+     *
+     *  2. 利用%与/ 来实现
+     *
+     *
+     *
+     * @param n
+     * @return
+     */
     public boolean test(int n) {
 
         if (n <= 0) {
@@ -29,6 +62,16 @@ public class IsPowerOfTwo {
     }
 
 
+    /*****
+     *    思路：
+     *      1: 时间复杂度为O(N) 严重超时
+     *
+     *      2：算法未通过
+     *
+     *
+     * @param n
+     * @return
+     */
     public boolean isPowerOfTwo(int n) {
 
         if (n <= 0) {
