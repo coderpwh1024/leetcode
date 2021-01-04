4d8c5630bcdd78332a6334b1e36f91d47b876b15
coderpwh <pwhfbc@outlook.com>
2021-01-04 09:41:46 +0800

    Add files via upload
    upload Fib.java

---
+++ src/main/java/com/coderpwh/leetcode/Fib.java
@@ -0,0 +1,70 @@
+package com.coderpwh.leetcode;
+
+/***
+ *
+ *  509 斐波那契数
+ *
+ *斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ *
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 给你 n ，请计算 F(n) 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ * 示例 2：
+ *
+ * 输入：3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ * 示例 3：
+ *
+ * 输入：4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ *
+ *
+ *
+ */
+public class Fib {
+    public static void main(String[] args) {
+
+        Fib fib = new Fib();
+//        fib.fib(4);
+        System.out.println(fib.fib(6));
+
+    }
+
+
+    /****
+     *  思路:
+     *    1：递归方式
+     *    2: 时间复杂度为O(N^2)
+     *    3：空间复杂度为O(1)
+     *
+     * @param n
+     * @return
+     */
+    public int fib(int n) {
+
+        int f0 = 0;
+        int f1 = 1;
+
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        int sum = fib(n - 1) + fib(n - 2);
+
+        return sum;
+    }
+
+}
