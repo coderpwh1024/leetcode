6548a7e7ec80c7842fb88d3fe1fa05bdc90e7d47
coderpwh <pwhfbc@outlook.com>
2021-01-07 16:56:39 +0800

    Update IsPalindrome.java

--- src/main/java/com/coderpwh/leetcode/IsPalindrome.java
+++ src/main/java/com/coderpwh/leetcode/IsPalindrome.java
@@ -1,6 +1,19 @@
-package com.coderpwh.leetcode;
+ package com.coderpwh.leetcode;
 
 /**
+ * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ * <p>
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: "race a car"
+ * 输出: false
+ *
  * @author coderpwh
  */
 public class IsPalindrome {
@@ -17,7 +30,11 @@ public class IsPalindrome {
 
 
     /***
-     *
+     *   思路:
+     *   1 验证回文串，首位字母正好相同（忽略大小写），除掉特殊字符
+     *   2 Character.isLetterOrDigit() 验证该字符是字母或者数字
+     *   3 空间复杂度为O(2)
+     *   4 时间复杂度为O(N) N 为s的长度
      *
      *
      *
