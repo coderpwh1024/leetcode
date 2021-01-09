bd61d18507e678dfc33785f7e7df8efd10274bda
coderpwh <pwhfbc@outlook.com>
2021-01-09 11:11:57 +0800

    Update IsValid.java

--- src/main/java/com/coderpwh/leetcode/IsValid.java
+++ src/main/java/com/coderpwh/leetcode/IsValid.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+
 public class IsValid {
 
     public static void main(String[] args) {
@@ -15,7 +16,17 @@ public class IsValid {
 
     }
 
-
+    /*****
+     *    思路:
+     *      1：时间复杂度为O(N),对长度为N的字符串进行遍历
+     *      2：空间复杂度为O(N+6) N为字符串的长度
+     *      3: 左右括号最终要闭环才能有效，把全部的左括号全部放入栈中
+     *         然后遍历栈，对栈顶元素进行匹配，匹配成功就是有效，不成功就是无效的
+     *         用map建立对应的key-value 关系
+     *
+     * @param s
+     * @return
+     */
     public boolean isValid(String s) {
 
         int n = s.length();
@@ -28,6 +39,7 @@ public class IsValid {
         map.put(')', '(');
         map.put(']', '[');
         map.put('}', '{');
+
 
         Deque<Character> deque = new LinkedList<Character>();
         for (int i = 0; i < n; i++) {
