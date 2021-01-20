118cbb97744e238c915cad0c7d4cfbf7efd5825c
coderpwh <pwhfbc@outlook.com>
2021-01-20 10:58:54 +0800

    Update MaximumProduct.java

--- src/main/java/com/coderpwh/leetcode/MaximumProduct.java
+++ src/main/java/com/coderpwh/leetcode/MaximumProduct.java
@@ -4,6 +4,23 @@ import java.io.Serializable;
 import java.util.*;
 
 /**
+ * 628 三个数的最大乘积
+ * <p>
+ * 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3]
+ * 输出: 6
+ * 示例 2:
+ * <p>
+ * 输入: [1,2,3,4]
+ * 输出: 24
+ * 注意:
+ * <p>
+ * 给定的整型数组长度范围是[3,104]，数组中所有的元素范围是[-1000, 1000]。
+ * 输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
+ *
  * @author coderpwh
  */
 public class MaximumProduct implements Serializable {
