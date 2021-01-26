8bf566c0afb2458ff683c71a0f1bfd4508444f1c
coderpwh <pwhfbc@outlook.com>
2021-01-26 15:53:03 +0800

    Update RemoveDuplicateLetters.java

--- src/main/java/com/coderpwh/leetcode/RemoveDuplicateLetters.java
+++ src/main/java/com/coderpwh/leetcode/RemoveDuplicateLetters.java
@@ -1,6 +1,23 @@
 package com.coderpwh.leetcode;
 
 /**
+ * 316 除去重复字母
+ * <p>
+ * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+ * <p>
+ * 注意：该题与 1081 https://leetcode-cn.com/problems/smallest-subsequence-of-distinct-characters 相同
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "bcabc"
+ * 输出："abc"
+ * 示例 2：
+ * <p>
+ * 输入：s = "cbacdcbc"
+ * 输出："acdb"
+ *
  * @author coderpwh
  */
 public class RemoveDuplicateLetters {
@@ -52,3 +69,4 @@ public class RemoveDuplicateLetters {
 
 
 }
+
