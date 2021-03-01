61a61ad9cacf9b61c2e4efbdcc9580bcbd02efc4
coderpwh <pwhfbc@outlook.com>
2021-03-01 10:03:10 +0800

    Update SortArrayByParity.java

--- src/main/java/com/coderpwh/leetcode/SortArrayByParity.java
+++ src/main/java/com/coderpwh/leetcode/SortArrayByParity.java
@@ -17,12 +17,60 @@ public class SortArrayByParity {
 
         SortArrayByParity sort = new SortArrayByParity();
 //        sort.sortArrayByParity(arr);
-        System.out.println(sort.sortArrayByParity(arr));
+//        System.out.println(sort.sortArrayByParity(arr));
 
+        sort.test(arr);
+
+    }
+
+    
+    
+
+    /****
+     *    思路:
+     *      1: 时间复杂度为O(N)
+     *      2: 空间复杂度为O(N) 多创建了数组空间
+     *      3: 2个for循环即可
+     *
+     *
+     *
+     * @param A
+     */
+    public void test(int[] A) {
+
+        int[] arr = new int[A.length];
+        int t = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0) {
+                arr[t++] = A[i];
+            }
+        }
+
+
+        for (int j = 0; j < A.length; j++) {
+            if (A[j] % 2 != 0) {
+                arr[t++] = A[j];
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
     }
 
 
+    /****
+     *    思路：
+     *      1：时间复杂度为O(N^N)
+     *      2：空间复杂度为O(N)
+     *      3: for循环过多
+     *
+     *
+     * @param A
+     * @return
+     */
     public int[] sortArrayByParity(int[] A) {
 
 
