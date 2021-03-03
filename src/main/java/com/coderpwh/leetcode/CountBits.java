b688980c904792040a25f050d32c934a855fffcd
coderpwh <pwhfbc@outlook.com>
2021-03-03 10:01:15 +0800

    Update CountBits.java

--- src/main/java/com/coderpwh/leetcode/CountBits.java
+++ src/main/java/com/coderpwh/leetcode/CountBits.java
@@ -1,5 +1,20 @@
 package com.coderpwh.leetcode;
 
+/*****
+ *给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+ *
+ * 示例 1:
+ *
+ * 输入: 2
+ * 输出: [0,1,1]
+ * 示例 2:
+ *
+ * 输入: 5
+ * 输出: [0,1,1,2,1,2]
+ *
+ *
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +32,17 @@ public class CountBits {
 
     }
 
-
+    /****
+     *     思路:
+     *       1：时间复杂度为O(N)
+     *       2: 空间复杂度为O(N),创建了list集合
+     *       3: 暴力方法解决，直接调用Integer的api
+     *
+     *
+     *
+     * @param num
+     * @return
+     */
     public int[] countBits(int num) {
 
         List<Integer> list = new ArrayList<>();
