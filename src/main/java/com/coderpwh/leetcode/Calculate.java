21eb92dea16b8a5c5c393bcce6cf9e7a7b17ecfc
coderpwh <pwhfbc@outlook.com>
2021-03-11 11:04:54 +0800

    Update Calculate.java

--- src/main/java/com/coderpwh/leetcode/Calculate.java
+++ src/main/java/com/coderpwh/leetcode/Calculate.java
@@ -4,6 +4,27 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 /**
+ * 227 基本计算器 ||
+ * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+ * <p>
+ * 整数除法仅保留整数部分。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "3+2*2"
+ * 输出：7
+ * 示例 2：
+ * <p>
+ * 输入：s = " 3/2 "
+ * 输出：1
+ * 示例 3：
+ * <p>
+ * 输入：s = " 3+5 / 2 "
+ * 输出：5
+ *  
+ *
  * @author coderpwh
  */
 public class Calculate {
