0e1aa74fb0fc4b810e729bd820624a86890b037a
coderpwh <pwhfbc@outlook.com>
2021-04-12 10:43:40 +0800

    Update LargestNumber.java

--- src/main/java/com/coderpwh/leetcode/LargestNumber.java
+++ src/main/java/com/coderpwh/leetcode/LargestNumber.java
@@ -1,6 +1,30 @@
 package com.coderpwh.leetcode;
 
 /**
+ * 179. 最大数
+ * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
+ * <p>
+ * 注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [10,2]
+ * 输出："210"
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,30,34,5,9]
+ * 输出："9534330"
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1]
+ * 输出："1"
+ * 示例 4：
+ * <p>
+ * 输入：nums = [10]
+ * 输出："10"
+ *
  * @author coderpwh
  */
 public class LargestNumber {
@@ -18,7 +42,7 @@ public class LargestNumber {
 
         LargestNumber number = new LargestNumber();
         number.largestNumber(nums);
-        System.out.println(number.largestNumber(nums)); 
+        System.out.println(number.largestNumber(nums));
 
 
     }
@@ -70,3 +94,4 @@ public class LargestNumber {
 
 
 }
+
