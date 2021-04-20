5e0d9d3dc9c58a257828e36cbfdd9c30f7031261
coderpwh <pwhfbc@outlook.com>
2021-04-20 10:46:20 +0800

    Update FindLengthOfLCIS.java

--- src/main/java/com/coderpwh/leetcode/FindLengthOfLCIS.java
+++ src/main/java/com/coderpwh/leetcode/FindLengthOfLCIS.java
@@ -1,7 +1,5 @@
 package com.coderpwh.leetcode;
 
-import java.util.Deque;
-import java.util.LinkedList;
 
 /**
  * @author coderpwh
@@ -24,6 +22,22 @@ public class FindLengthOfLCIS {
     }
 
 
+    /***
+     *    思路:
+     *       1.时间复杂度为O(N)
+     *       2.空间复杂度为O(1)
+     *       3.主要是用贪心算法来实现的，连续递增序列要满足 nums[i]<nums[i+1]
+     *        长度为n的数组 0<l<=r<n-1,范围[l,r]是递增
+     *        则要满足 nums[l-1]<nums[l] ,nums[r-1]<nums[r]
+     *        定义了变量start = 0; 起始位置，如果 nums[i]<=nums[i-1] 则不满足递增序列的特征
+     *        重新开始记录位置下标
+     *
+     *
+     *
+     * @param nums
+     * @return
+     */
+
     public int findLengthOfLCIS(int[] nums) {
 
         int ans = 0;
@@ -33,7 +47,7 @@ public class FindLengthOfLCIS {
 
         for (int i = 0; i < n; i++) {
 
-            if (i > 0 && nums[i] <=nums[i - 1]) {
+            if (i > 0 && nums[i] <= nums[i - 1]) {
                 start = i;
             }
             ans = Math.max(ans, i - start + 1);
