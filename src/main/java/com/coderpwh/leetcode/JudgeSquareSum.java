75aa459ebd05c47d1978aeff8f66d391d93312d2
coderpwh <pwhfbc@outlook.com>
2021-04-28 10:40:45 +0800

    Update JudgeSquareSum.java

--- src/main/java/com/coderpwh/leetcode/JudgeSquareSum.java
+++ src/main/java/com/coderpwh/leetcode/JudgeSquareSum.java
@@ -13,6 +13,7 @@ public class JudgeSquareSum {
         JudgeSquareSum sum = new JudgeSquareSum();
 //        sum.judgeSquareSum(num);
         System.out.println(sum.judgeSquareSum(num));
+        System.out.println(sum.test(num));
     }
 
 
@@ -38,4 +39,27 @@ public class JudgeSquareSum {
         return false;
     }
 
+
+    /***
+     *   思路:
+     *      1. 时间复杂度为O(c开根号)
+     *      2. 空间复杂度为O(1)
+     *      3. 利用sqrt 函数
+     *
+     * @param c
+     * @return
+     */
+    public boolean test(int c) {
+
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int) b) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
+
