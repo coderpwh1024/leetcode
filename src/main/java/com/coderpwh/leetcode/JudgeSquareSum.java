267b130001d4bc7731b44b4da927840c25d95004
coderpwh <pwhfbc@outlook.com>
2021-04-28 10:44:50 +0800

    Update JudgeSquareSum.java

--- src/main/java/com/coderpwh/leetcode/JudgeSquareSum.java
+++ src/main/java/com/coderpwh/leetcode/JudgeSquareSum.java
@@ -1,6 +1,34 @@
 package com.coderpwh.leetcode;
 
 /**
+ * 633 平方数之和
+ * <p>
+ * 给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：c = 5
+ * 输出：true
+ * 解释：1 * 1 + 2 * 2 = 5
+ * 示例 2：
+ * <p>
+ * 输入：c = 3
+ * 输出：false
+ * 示例 3：
+ * <p>
+ * 输入：c = 4
+ * 输出：true
+ * 示例 4：
+ * <p>
+ * 输入：c = 2
+ * 输出：true
+ * 示例 5：
+ * <p>
+ * 输入：c = 1
+ * 输出：true
+ *
  * @author coderpwh
  */
 public class JudgeSquareSum {
@@ -62,4 +90,3 @@ public class JudgeSquareSum {
 
 
 }
-
