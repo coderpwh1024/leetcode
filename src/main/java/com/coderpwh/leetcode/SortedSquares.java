9f7ea468e4585f405f7e4362c904edc72453a343
coderpwh <pwhfbc@outlook.com>
2021-05-21 10:21:31 +0800

    Update SortedSquares.java

--- src/main/java/com/coderpwh/leetcode/SortedSquares.java
+++ src/main/java/com/coderpwh/leetcode/SortedSquares.java
@@ -4,6 +4,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
+/****
+ *
+ *  977 有序数组的平方
+ *
+ * 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-4,-1,0,3,10]
+ * 输出：[0,1,9,16,100]
+ * 解释：平方后，数组变为 [16,1,0,9,100]
+ * 排序后，数组变为 [0,1,9,16,100]
+ * 示例 2：
+ *
+ * 输入：nums = [-7,-3,2,3,11]
+ * 输出：[4,9,9,49,121]
+ *
+ *
+ *
+ */
 public class SortedSquares {
 
     public static void main(String[] args) {
@@ -57,3 +80,4 @@ public class SortedSquares {
 
 
 }
+
