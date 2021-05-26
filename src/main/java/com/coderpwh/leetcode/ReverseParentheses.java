f420bc6211de44738b38377b2536aa30247f26f2
coderpwh <pwhfbc@outlook.com>
2021-05-26 10:47:06 +0800

    Update ReverseParentheses.java

--- src/main/java/com/coderpwh/leetcode/ReverseParentheses.java
+++ src/main/java/com/coderpwh/leetcode/ReverseParentheses.java
@@ -4,6 +4,31 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 /**
+ * 给出一个字符串 s（仅含有小写英文字母和括号）。
+ * <p>
+ * 请你按照从括号内到外的顺序，逐层反转每对匹配括号中的字符串，并返回最终的结果。
+ * <p>
+ * 注意，您的结果中 不应 包含任何括号。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "(abcd)"
+ * 输出："dcba"
+ * 示例 2：
+ * <p>
+ * 输入：s = "(u(love)i)"
+ * 输出："iloveu"
+ * 示例 3：
+ * <p>
+ * 输入：s = "(ed(et(oc))el)"
+ * 输出："leetcode"
+ * 示例 4：
+ * <p>
+ * 输入：s = "a(bcdefghijkl(mno)p)q"
+ * 输出："apmnolkjihgfedcbq"
+ *
  * @author coderpwh
  */
 public class ReverseParentheses {
@@ -47,3 +72,4 @@ public class ReverseParentheses {
 
 
 }
+
