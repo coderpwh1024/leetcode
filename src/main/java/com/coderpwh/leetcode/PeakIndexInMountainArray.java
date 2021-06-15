9efcd0e4b99b6ffbed2168ca511f89893895ba77
coderpwh <pwhfbc@outlook.com>
2021-06-15 11:05:52 +0800

    Update PeakIndexInMountainArray.java

--- src/main/java/com/coderpwh/leetcode/PeakIndexInMountainArray.java
+++ src/main/java/com/coderpwh/leetcode/PeakIndexInMountainArray.java
@@ -1,7 +1,38 @@
 package com.coderpwh.leetcode;
 
 
-import java.util.Arrays;
+/***
+ *
+ *符合下列属性的数组 arr 称为 山脉数组 ：
+ * arr.length >= 3
+ * 存在 i（0 < i < arr.length - 1）使得：
+ * arr[0] < arr[1] < ... arr[i-1] < arr[i]
+ * arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+ * 给你由整数组成的山脉数组 arr ，返回任何满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1] 的下标 i 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：arr = [0,1,0]
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：arr = [0,2,1,0]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：arr = [0,10,5,2]
+ * 输出：1
+ * 示例 4：
+ *
+ * 输入：arr = [3,4,5,1]
+ * 输出：2
+ * 示例 5：
+ *
+ * 输入：arr = [24,69,100,99,79,78,67,36,26,19]
+ * 输出：2
+ */
 
 public class PeakIndexInMountainArray {
 
@@ -38,3 +69,4 @@ public class PeakIndexInMountainArray {
     }
 
 }
+
