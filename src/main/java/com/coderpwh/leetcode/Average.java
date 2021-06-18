b05652a7b0e763154d17884005c127955e67b3ac
coderpwh <pwhfbc@outlook.com>
2021-06-18 10:21:34 +0800

    Add files via upload
    upload Averrage.java file

---
+++ src/main/java/com/coderpwh/leetcode/Average.java
@@ -0,0 +1,42 @@
+package com.coderpwh.leetcode;
+
+public class Average {
+
+
+    public static void main(String[] args) {
+
+
+//        int[] arr = {4000, 3000, 1000, 2000};
+
+//        int[] arr = {8000, 9000, 2000, 3000, 6000, 1000};
+
+        int arr[] = {48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000};
+
+        Average average = new Average();
+//        average.average(arr);
+
+        System.out.println(average.average(arr));
+
+    }
+
+
+    public double average(int[] salary) {
+
+
+        double max = Integer.MIN_VALUE;
+
+        double min = Integer.MAX_VALUE;
+
+        double sum = 0;
+        for (Integer num : salary) {
+            sum += num;
+
+            max = Math.max(num, max);
+            min = Math.min(num, min);
+        }
+
+        return (sum - max - min) / (salary.length - 2);
+
+
+    }
+}
