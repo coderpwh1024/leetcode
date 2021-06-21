2d0e255c8d791325134c9fe30ac1ec390cc66c76
coderpwh <pwhfbc@outlook.com>
2021-06-21 10:34:35 +0800

    Update Intersection.java

--- src/main/java/com/coderpwh/leetcode/Intersection.java
+++ src/main/java/com/coderpwh/leetcode/Intersection.java
@@ -3,6 +3,27 @@ package com.coderpwh.leetcode;
 import java.util.HashSet;
 import java.util.Set;
 
+
+/***
+ *
+ * 349. 两个数组的交集
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 示例 2：
+ *
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ *
+ *
+ *
+ */
 public class Intersection {
 
 
@@ -23,7 +44,7 @@ public class Intersection {
 
 
     /***
-     *    思路: 
+     *    思路:
      *        1.时间复杂度为O(N^2)
      *        2. 空间复杂度为O(N)
      *        3. for循环遍历
