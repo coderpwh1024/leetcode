d4bb71c1af5aeec8a155433f4241706d6cf24ab8
coderpwh <pwhfbc@outlook.com>
2021-06-29 10:27:15 +0800

    Update ConvertToTitle.java

--- src/main/java/com/coderpwh/leetcode/ConvertToTitle.java
+++ src/main/java/com/coderpwh/leetcode/ConvertToTitle.java
@@ -1,5 +1,34 @@
 package com.coderpwh.leetcode;
 
+
+/***
+ * 
+ * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+ *
+ * 例如，
+ *
+ *     1 -> A
+ *     2 -> B
+ *     3 -> C
+ *     ...
+ *     26 -> Z
+ *     27 -> AA
+ *     28 -> AB
+ *     ...
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: "A"
+ * 示例 2:
+ *
+ * 输入: 28
+ * 输出: "AB"
+ * 示例 3:
+ *
+ * 输入: 701
+ * 输出: "ZY"
+ *
+ */
 public class ConvertToTitle {
 
     public static void main(String[] args) {
@@ -48,3 +77,4 @@ public class ConvertToTitle {
 
 
 }
+
