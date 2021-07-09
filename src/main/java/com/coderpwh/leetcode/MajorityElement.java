c9c61a8150e9c8e3d169ed59053f2cee4d2e5903
coderpwh <pwhfbc@outlook.com>
2021-07-09 09:56:50 +0800

    Update MajorityElement.java

--- src/main/java/com/coderpwh/leetcode/MajorityElement.java
+++ src/main/java/com/coderpwh/leetcode/MajorityElement.java
@@ -29,16 +29,30 @@ public class MajorityElement {
     public static void main(String[] args) {
 
 //        int[] nums = {2, 2, 1, 1, 1, 2, 2};
-        int [] nums = {3,2,3};
+//        int [] nums = {3,2,3};
+
+        int[] nums = {1, 2, 5, 9, 5, 9, 5, 5, 5};
 
         MajorityElement element = new MajorityElement();
         System.out.println(element.majorityElement(nums));
     }
 
 
+    /***
+     *
+     *     思路:
+     *        1. 时间复杂度为O(N)
+     *        2. 空间复杂度为O(1)
+     *        3. 利用哈希 进行遍历实现
+     *
+     *
+     *
+     * @param nums
+     * @return
+     */
     public int majorityElement(int[] nums) {
 
-         int sum = nums.length/2;
+        int sum = nums.length / 2;
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
@@ -51,12 +65,12 @@ public class MajorityElement {
             }
         }
 
-        for(Integer key:map.keySet()){
-            if(map.get(key)>sum){
-                return  key;
+        for (Integer key : map.keySet()) {
+            if (map.get(key) > sum) {
+                return key;
             }
         }
-        return  0;
+        return 0;
 
     }
 
