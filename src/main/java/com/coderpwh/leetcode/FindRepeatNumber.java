eeb2d159eee4309dac36f1486101f621a6915765
coderpwh <pwhfbc@outlook.com>
2021-07-20 10:23:35 +0800

    Update FindRepeatNumber.java

--- src/main/java/com/coderpwh/leetcode/FindRepeatNumber.java
+++ src/main/java/com/coderpwh/leetcode/FindRepeatNumber.java
@@ -4,6 +4,24 @@ import javax.print.attribute.standard.NumberUp;
 import java.util.HashMap;
 import java.util.Map;
 
+/***
+ * 找出数组中重复的数字。
+ *
+ *
+ * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * [2, 3, 1, 0, 2, 5, 3]
+ * 输出：2 或 3 
+ *  
+ *
+ * 限制：
+ *
+ * 2 <= n <= 100000
+ *
+ */
 public class FindRepeatNumber {
 
 
@@ -48,7 +66,7 @@ public class FindRepeatNumber {
 
             if (map.get(count) > 1) {
                 System.out.println(count);
-                return  count;
+                return count;
             }
         }
 
@@ -59,4 +77,3 @@ public class FindRepeatNumber {
 
 
 }
-
