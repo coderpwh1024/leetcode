82616ba53923d35e292f216563012c686fd9c273
coderpwh <pwhfbc@outlook.com>
2021-07-22 11:52:36 +0800

    Update Intersect.java

--- src/main/java/com/coderpwh/leetcode/Intersect.java
+++ src/main/java/com/coderpwh/leetcode/Intersect.java
@@ -39,7 +39,7 @@ public class Intersect {
 
 
         Intersect intersect = new Intersect();
-        int[] arr = intersect.intersect(nums1, nums2);
+        int[] arr = intersect.test(nums1, nums2);
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + "  ");
@@ -100,5 +100,41 @@ public class Intersect {
         return Arrays.copyOfRange(intersection, 0, index);
     }
 
-}
 
+    public int[] test(int[] nums1, int[] nums2) {
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int length1 = nums1.length;
+
+        int length2 = nums2.length;
+
+        int[] intersection = new int[Math.min(length1, length1)];
+
+        int index1 = 0;
+
+        int index2 = 0;
+
+        int index = 0;
+
+        while (index1 < length1 && index2 < length2) {
+
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                intersection[index] = nums1[index1];
+                index1++;
+                index2++;
+                index++;
+            }
+
+        }
+
+
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+}
