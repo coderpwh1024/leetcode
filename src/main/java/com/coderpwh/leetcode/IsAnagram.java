9f50f80a3c1f0ad57abdc8859bb72d0b38256f51
coderpwh <pwhfbc@outlook.com>
2021-07-26 11:38:16 +0800

    Update IsAnagram.java

--- src/main/java/com/coderpwh/leetcode/IsAnagram.java
+++ src/main/java/com/coderpwh/leetcode/IsAnagram.java
@@ -2,6 +2,28 @@ package com.coderpwh.leetcode;
 
 import java.util.HashMap;
 
+/***
+ *
+ *  242 有效的字母异位词
+ *
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ *
+ *
+ *
+ */
 public class IsAnagram {
 
     public static void main(String[] args) {
@@ -57,7 +79,6 @@ public class IsAnagram {
             if (count <= 0) {
                 return false;
             }
-
             count -= 1;
             map.put(c, count);
         }
@@ -66,3 +87,4 @@ public class IsAnagram {
     }
 
 }
+
