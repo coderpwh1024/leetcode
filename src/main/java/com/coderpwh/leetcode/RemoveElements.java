620c236994a2c20b5562d9a6fe368de61834207e
coderpwh <pwhfbc@outlook.com>
2021-07-27 14:24:51 +0800

    Update RemoveElements.java

--- src/main/java/com/coderpwh/leetcode/RemoveElements.java
+++ src/main/java/com/coderpwh/leetcode/RemoveElements.java
@@ -1,5 +1,30 @@
 package com.coderpwh.leetcode;
 
+
+/***
+ *
+ * 203 移除链表元素
+ *
+ *给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,6,3,4,5,6], val = 6
+ * 输出：[1,2,3,4,5]
+ * 示例 2：
+ *
+ * 输入：head = [], val = 1
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：head = [7,7,7,7], val = 7
+ * 输出：[]
+ *
+ *
+ *
+ */
 public class RemoveElements {
 
     public static void main(String[] args) {
@@ -30,21 +55,22 @@ public class RemoveElements {
 
         ListNode headNode = new ListNode(0);
 
-        headNode.next=head;
+        headNode.next = head;
 
-        ListNode temp =headNode;
+        ListNode temp = headNode;
 
-        while (temp.next!=null){
+        while (temp.next != null) {
 
-            if(temp.next.val==val){
-             temp.next=temp.next.next;
-            }else{
-                temp=temp.next;
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
             }
         }
 
-        return  headNode.next;
+        return headNode.next;
     }
 
 
 }
+
