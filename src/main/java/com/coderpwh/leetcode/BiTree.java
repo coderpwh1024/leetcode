87440ca02d0021eebdb020aa85c851d2d3a91c82
coderpwh <pwhfbc@outlook.com>
2021-07-29 12:00:24 +0800

    Add files via upload
    upload  BiTree.java file

---
+++ src/main/java/com/coderpwh/leetcode/BiTree.java
@@ -0,0 +1,26 @@
+package com.coderpwh.leetcode;
+
+public class BiTree {
+
+
+    private TreeNode root;
+
+
+    public BiTree() {
+        this.root = null;
+    }
+
+
+    public BiTree(TreeNode root) {
+        this.root = root;
+    }
+
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+}
