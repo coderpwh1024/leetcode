8b7a4b66dcbd2d8aea96b57e0c745cfb5d4302e8
coderpwh <pwhfbc@outlook.com>
2021-08-03 10:05:52 +0800

    Update MaxDepth.java

--- src/main/java/com/coderpwh/leetcode/MaxDepth.java
+++ src/main/java/com/coderpwh/leetcode/MaxDepth.java
@@ -7,6 +7,42 @@ public class MaxDepth {
 
         MaxDepth maxDepth = new MaxDepth();
 
+        // 创建二叉树
+        BiTree biTree = maxDepth.createBTree();
+
+        // 获取根
+        TreeNode root = biTree.getRoot();
+
+
+    }
+
+
+    /***
+     * 创建二叉树
+     *
+     * @return
+     */
+    public BiTree createBTree() {
+/*
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2, node3, null);
+
+        TreeNode node1 = new TreeNode(1, null, node2);*/
+
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node6 = new TreeNode(6);
+
+        TreeNode node3 = new TreeNode(3, node6, node7);
+
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(4);
+
+        TreeNode node2 = new TreeNode(2, node4, node5);
+
+        TreeNode node1 = new TreeNode(1, node2, node3);
+
+
+        return new BiTree(node1);
 
     }
 
