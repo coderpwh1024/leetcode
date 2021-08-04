3e9aa45a4a568b01ad31332c80fd625cf31ba581
coderpwh <pwhfbc@outlook.com>
2021-08-04 11:32:13 +0800

    Update TriangleNumber.java

--- src/main/java/com/coderpwh/leetcode/TriangleNumber.java
+++ src/main/java/com/coderpwh/leetcode/TriangleNumber.java
@@ -1,5 +1,6 @@
 package com.coderpwh.leetcode;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.Arrays;
 
 public class TriangleNumber {
@@ -16,23 +17,40 @@ public class TriangleNumber {
     }
 
 
+    /***
+     *
+     *     思路:
+     *        1. 时间复杂度为O(N^2)
+     *        2. 空间复杂度为O(logN)
+     *        3. 利用双指针方式实现
+     *
+     *
+     * @param nums
+     * @return
+     */
     public int triangleNumber(int[] nums) {
 
         int n = nums.length;
+
         Arrays.sort(nums);
 
         int ans = 0;
 
         for (int i = 0; i < n; i++) {
+
             int k = i;
+
             for (int j = i + 1; j < n; j++) {
+
                 while (k + 1 < n && nums[k + 1] < nums[i] + nums[j]) {
                     ++k;
                 }
                 ans += Math.max(k - j, 0);
             }
         }
+
         System.out.println(ans);
+
         return ans;
     }
 
