75cf60593d4676dbcd3050e98f792b32b0c86335
coderpwh <pwhfbc@outlook.com>
2021-08-09 14:18:41 +0800

    Add files via upload
    upload  LevelOrder.java file

---
+++ src/main/java/com/coderpwh/leetcode/LevelOrder.java
@@ -0,0 +1,100 @@
+package com.coderpwh.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder {
+
+
+    public static void main(String[] args) {
+
+        LevelOrder levelOrder = new LevelOrder();
+
+
+        BiTree biTree = levelOrder.createBTree();
+
+
+        // 根结点
+        TreeNode root = biTree.getRoot();
+
+
+        levelOrder.levelOrder(root);
+
+    }
+
+    /***
+     *
+     * 创建二叉树
+     *
+     * @return
+     */
+    public BiTree createBTree() {
+
+        TreeNode node7 = new TreeNode(5 + "");
+        TreeNode node6 = new TreeNode(null);
+
+        TreeNode node3 = new TreeNode(3 + "", null, node7);
+
+        TreeNode node5 = new TreeNode(null);
+        TreeNode node4 = new TreeNode(4 + "");
+
+        TreeNode node2 = new TreeNode(2 + "", node4,null);
+
+        TreeNode node1 = new TreeNode(1 + "", node2, node3);
+
+
+        return new BiTree(node1);
+    }
+
+    public List<List<String>> levelOrder(TreeNode root) {
+
+        List<List<String>> list = new ArrayList<>();
+        level(root, list);
+
+        if (list != null && list.size() > 0) {
+
+            for (List<String> date : list) {
+                System.out.println(date);
+            }
+        }
+
+        return list;
+    }
+
+    public void level(TreeNode root, List<List<String>> list) {
+
+
+        if (root != null) {
+
+            LinkedList<TreeNode> L = new LinkedList<TreeNode>();
+
+            L.offer(root);
+            List<String> rootList = new ArrayList<>();
+            rootList.add(root.val);
+            list.add(rootList);
+
+
+            while (!L.isEmpty()) {
+
+                root = L.poll();
+                List<String> nodeList = new ArrayList<>();
+                if (root.left != null) {
+                    L.offer(root.left);
+                    nodeList.add(root.left.val);
+                }
+
+                if (root.right != null) {
+                    L.offer(root.right);
+                    nodeList.add(root.right.val);
+                }
+                if (nodeList != null && nodeList.size() > 0) {
+                    list.add(nodeList);
+                }
+            }
+        }
+    }
+
+
+}
