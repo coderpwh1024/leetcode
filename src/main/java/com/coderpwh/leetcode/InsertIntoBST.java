5d7159b0e073e8f1b05cfd335d17f9deb4c6877a
coderpwh <pwhfbc@outlook.com>
2021-08-12 11:43:33 +0800

    Update InsertIntoBST.java

--- src/main/java/com/coderpwh/leetcode/InsertIntoBST.java
+++ src/main/java/com/coderpwh/leetcode/InsertIntoBST.java
@@ -1,8 +1,38 @@
-package com.coderpwh.leetcode;
+ package com.coderpwh.leetcode;
 
 public class InsertIntoBST {
 
     public static void main(String[] args) {
+
+        int val = 5;
+
+        InsertIntoBST insertIntoBST = new InsertIntoBST();
+
+        BiTrees biTrees = insertIntoBST.create();
+
+
+        insertIntoBST.insertIntoBST(biTrees.getRoot(),val);
+
+        insertIntoBST.preNode(biTrees.getRoot());
+
+
+    }
+
+
+    public BiTrees create() {
+
+        TreeNodes node1 = new TreeNodes(1);
+
+        TreeNodes node3 = new TreeNodes(3);
+
+        TreeNodes node2 = new TreeNodes(2, node1, node3);
+
+        TreeNodes node7 = new TreeNodes(7);
+
+        TreeNodes node4 = new TreeNodes(4, node2, node7);
+
+        return new BiTrees(node4);
+
 
     }
 
@@ -36,6 +66,18 @@ public class InsertIntoBST {
             }
         }
         return root;
+    }
+
+
+    public void preNode(TreeNodes node) {
+
+        if (node != null) {
+            System.out.println(node.val);
+            preNode(node.left);
+            preNode(node.right);
+        }
+
+
     }
 
 
