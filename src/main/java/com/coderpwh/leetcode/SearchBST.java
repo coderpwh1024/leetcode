be2d3ce728c80ca587668611edb54a1c87e0d142
coderpwh <pwhfbc@outlook.com>
2021-08-12 10:41:15 +0800

    Update SearchBST.java

--- src/main/java/com/coderpwh/leetcode/SearchBST.java
+++ src/main/java/com/coderpwh/leetcode/SearchBST.java
@@ -1,10 +1,52 @@
- package com.coderpwh.leetcode;
+package com.coderpwh.leetcode;
 
 public class SearchBST {
 
     public static void main(String[] args) {
 
+
+        String val = "2";
+
         SearchBST bst = new SearchBST();
+
+        // 创建二叉树
+        BiTree biTree = bst.createTree();
+
+        // 获取根结点
+        TreeNode root = biTree.getRoot();
+
+
+        //  搜索二叉树上的值
+        TreeNode node = bst.searchBST(root, val);
+
+        System.out.println(node.val);
+
+        System.out.println(node.left.val);
+
+        System.out.println(node.right.val);
+
+
+    }
+
+
+    /**
+     * 创建二叉树
+     *
+     * @return
+     */
+    public BiTree createTree() {
+
+
+        TreeNode node1 = new TreeNode("1");
+        TreeNode node3 = new TreeNode("3");
+
+        TreeNode node2 = new TreeNode("2", node1, node3);
+
+        TreeNode node7 = new TreeNode("7");
+
+        TreeNode node4 = new TreeNode("4", node2, node7);
+
+        return new BiTree(node4);
 
 
     }
@@ -21,7 +63,7 @@ public class SearchBST {
      * @param val
      * @return
      */
-    public TreeNode searchBST(TreeNode root, int val) {
+    public TreeNode searchBST(TreeNode root, String val) {
 
 
         if (root == null) {
