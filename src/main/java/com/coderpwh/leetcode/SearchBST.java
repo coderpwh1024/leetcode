b2e3f5773fa6c1f82c1b304d9c89958d89a4b96d
coderpwh <pwhfbc@outlook.com>
2021-08-12 10:43:23 +0800

    Update SearchBST.java

--- src/main/java/com/coderpwh/leetcode/SearchBST.java
+++ src/main/java/com/coderpwh/leetcode/SearchBST.java
@@ -1,5 +1,29 @@
-package com.coderpwh.leetcode;
+  package com.coderpwh.leetcode;
 
+/***
+ *  700 二叉树搜索树种的值
+ *
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ *
+ * 例如，
+ *
+ * 给定二叉搜索树:
+ *
+ *         4
+ *        / \
+ *       2   7
+ *      / \
+ *     1   3
+ *
+ * 和值: 2
+ * 你应该返回如下子树:
+ *
+ *       2
+ *      / \
+ *     1   3
+ *
+ *
+ */
 public class SearchBST {
 
     public static void main(String[] args) {
