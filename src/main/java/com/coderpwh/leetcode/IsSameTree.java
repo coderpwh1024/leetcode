a0de329db3d96523d29a034ea0f6e5f06bae0169
coderpwh <pwhfbc@outlook.com>
2021-08-17 11:07:48 +0800

    Update IsSameTree.java

--- src/main/java/com/coderpwh/leetcode/IsSameTree.java
+++ src/main/java/com/coderpwh/leetcode/IsSameTree.java
@@ -4,7 +4,60 @@ public class IsSameTree {
 
     public static void main(String[] args) {
 
+
+        IsSameTree tree = new IsSameTree();
+
+
+        // 调用创建二叉树
+        BiTrees biTrees1 = tree.create();
+
+        // 获取二叉树根结点
+        TreeNodes p = biTrees1.getRoot();
+
+        //调用创建二叉树
+        BiTrees biTrees2 = tree.create2();
+
+        // 获取二叉树根结点
+        TreeNodes q = biTrees2.getRoot();
+
+
+        System.out.println(tree.isSameTree(p, q));
+
+
     }
+
+
+    /***
+     * 创建二叉树
+     * @return
+     */
+    public BiTrees create() {
+
+        TreeNodes node2 = new TreeNodes(2);
+
+        TreeNodes node3 = new TreeNodes(3);
+
+        TreeNodes node1 = new TreeNodes(1, node2, node3);
+
+        return new BiTrees(node1);
+    }
+
+
+    /***
+     *   创建二叉树
+     * @return
+     */
+    public BiTrees create2() {
+
+        TreeNodes node2 = new TreeNodes(2);
+
+        TreeNodes node3 = new TreeNodes(3);
+
+        TreeNodes node1 = new TreeNodes(1, node2, node3);
+
+        return new BiTrees(node1);
+    }
+
 
     public boolean isSameTree(TreeNodes p, TreeNodes q) {
 
