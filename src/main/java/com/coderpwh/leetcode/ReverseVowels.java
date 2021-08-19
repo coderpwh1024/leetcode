ed586eea3f3af8fda286a4e88ef528e86c4e6aa9
coderpwh <pwhfbc@outlook.com>
2021-08-19 10:33:47 +0800

    Add files via upload
    upload ReverseVowels.java file

---
+++ src/main/java/com/coderpwh/leetcode/ReverseVowels.java
@@ -0,0 +1,60 @@
+package com.coderpwh.leetcode;
+
+public class ReverseVowels {
+
+
+    public static void main(String[] args) {
+
+
+
+        String s = "hello";
+
+         ReverseVowels reverseVowels = new ReverseVowels();
+
+        System.out.println(reverseVowels.reverseVowels(s));
+
+    }
+
+
+    public String reverseVowels(String s) {
+
+        int n = s.length();
+
+        char[] arr = s.toCharArray();
+
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j) {
+            while (i < n && !isVowel(arr[i])) {
+                ++i;
+            }
+            while (j > 0 && !isVowel(arr[j])) {
+                --j;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+                ++i;
+                --j;
+            }
+        }
+
+
+        return new String(arr);
+    }
+
+    public boolean isVowel(char ch) {
+        return "aeiouAEIOU".indexOf(ch) >= 0;
+    }
+
+    public void swap(char[] arr, int i, int j) {
+
+        char temp = arr[i];
+
+        arr[i] = arr[j];
+
+        arr[j] = temp;
+    }
+
+
+}
