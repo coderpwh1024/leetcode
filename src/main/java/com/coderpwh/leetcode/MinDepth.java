49702ccfa3cbce0fc85b33b7fbd5766e76c7bed3
coderpwh <pwhfbc@outlook.com>
2021-08-20 12:00:22 +0800

    Add files via upload
    upload MinDepth.java file

---
+++ src/main/java/com/coderpwh/leetcode/MinDepth.java
@@ -0,0 +1,62 @@
+package com.coderpwh.leetcode;
+
+public class MinDepth {
+
+    public static void main(String[] args) {
+
+
+        MinDepth depth = new MinDepth();
+
+        BiTrees biTrees = depth.create();
+
+        TreeNodes root = biTrees.getRoot();
+
+        System.out.println(depth.minDepth(root));
+
+
+    }
+
+
+    public BiTrees create() {
+
+        TreeNodes node9 = new TreeNodes(9);
+
+        TreeNodes node15 = new TreeNodes(15);
+
+        TreeNodes node7 = new TreeNodes(7);
+
+        TreeNodes node20 = new TreeNodes(20, node15, node7);
+
+        TreeNodes nodes3 = new TreeNodes(3, node9, node20);
+
+        return new BiTrees(nodes3);
+    }
+
+
+    public int minDepth(TreeNodes root) {
+
+
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int min_depth = Integer.MAX_VALUE;
+
+
+        if (root.left != null) {
+            min_depth = Math.min(minDepth(root.left), min_depth);
+        }
+
+        if (root.right != null) {
+            min_depth = Math.min(minDepth(root.right), min_depth);
+        }
+
+
+        return min_depth + 1;
+    }
+
+}
