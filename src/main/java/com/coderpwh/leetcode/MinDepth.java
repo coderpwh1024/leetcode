ec47944524c6ce69a8b3c1343a5662a06d7039bd
coderpwh <pwhfbc@outlook.com>
2021-08-20 12:04:33 +0800

    Update MinDepth.java

--- src/main/java/com/coderpwh/leetcode/MinDepth.java
+++ src/main/java/com/coderpwh/leetcode/MinDepth.java
@@ -1,5 +1,31 @@
  package com.coderpwh.leetcode;
 
+
+/****
+ *
+ * 111. 二叉树的最小深度
+ *
+ *
+ * 给定一个二叉树，找出其最小深度。
+ *
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明：叶子节点是指没有子节点的节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：2
+ * 示例 2：
+ *
+ * 输入：root = [2,null,3,null,4,null,5,null,6]
+ * 输出：5
+ *
+ *
+ */
 public class MinDepth {
 
     public static void main(String[] args) {
