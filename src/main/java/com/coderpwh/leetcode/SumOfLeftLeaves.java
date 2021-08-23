3dbb76951f75d56aff4683c51f4c2aa4de999ba9
coderpwh <pwhfbc@outlook.com>
2021-08-23 10:49:41 +0800

    Update SumOfLeftLeaves.java

--- src/main/java/com/coderpwh/leetcode/SumOfLeftLeaves.java
+++ src/main/java/com/coderpwh/leetcode/SumOfLeftLeaves.java
@@ -1,6 +1,20 @@
- package com.coderpwh.leetcode;
+package com.coderpwh.leetcode;
 
 /**
+ * 404 左叶子之和
+ * <p>
+ * 计算给定二叉树的所有左叶子之和。
+ * <p>
+ * 示例：
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ *
  * @author coderpwh
  */
 public class SumOfLeftLeaves {
