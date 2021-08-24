d4bf407af92cce2897fa3e428dbb3835d45be3a3
coderpwh <pwhfbc@outlook.com>
2021-08-24 11:21:22 +0800

    Update FindMode.java

--- src/main/java/com/coderpwh/leetcode/FindMode.java
+++ src/main/java/com/coderpwh/leetcode/FindMode.java
@@ -3,6 +3,30 @@ package com.coderpwh.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ *
+ *  501 二叉搜索树种的众数
+ *
+ *  给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）
+ *
+ * 假定 BST 有如下定义：
+ *
+ * 结点左子树中所含结点的值小于等于当前结点的值
+ * 结点右子树中所含结点的值大于等于当前结点的值
+ * 左子树和右子树都是二叉搜索树
+ * 例如：
+ * 给定 BST [1,null,2,2],
+ *
+ *    1
+ *     \
+ *      2
+ *     /
+ *    2
+ *
+ *
+ *
+ *
+ */
 public class FindMode {
 
 
@@ -15,10 +39,10 @@ public class FindMode {
         FindMode findMode = new FindMode();
 
 
-         // 创建二叉树
+        // 创建二叉树
         BiTrees biTrees = findMode.create();
 
-         // 获取根部
+        // 获取根部
         TreeNodes root = biTrees.getRoot();
 
 
@@ -104,3 +128,4 @@ public class FindMode {
     }
 
 }
+
