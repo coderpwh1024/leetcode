cd4feda5ab1e8ae15bb5f4276fc99ef28c70c48e
coderpwh <pwhfbc@outlook.com>
2021-08-25 10:34:52 +0800

    Update LevelOrderBottom.java

--- src/main/java/com/coderpwh/leetcode/LevelOrderBottom.java
+++ src/main/java/com/coderpwh/leetcode/LevelOrderBottom.java
@@ -1,7 +1,30 @@
- package com.coderpwh.leetcode;
+package com.coderpwh.leetcode;
 
 import java.util.*;
 
+/***
+ *
+ * 给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其自底向上的层序遍历为：
+ *
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ *
+ *
+ *
+ */
 public class LevelOrderBottom {
 
     public static void main(String[] args) {
