d4e7b9e138ea6f8c7565ce864ea76c51f9566872
coderpwh <pwhfbc@outlook.com>
2021-08-26 11:56:26 +0800

    Add files via upload
    upload SortedLIstToBST.java file

---
+++ src/main/java/com/coderpwh/leetcode/SortedListToBST.java
@@ -0,0 +1,76 @@
+package com.coderpwh.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortedListToBST {
+
+    public static void main(String[] args) {
+
+        SortedListToBST bst = new SortedListToBST();
+
+        ListNode listNode = bst.getListNode();
+
+        bst.printNode(listNode);
+
+
+    }
+
+
+    public TreeNodes sortedListToBST(ListNode head) {
+
+        List<Integer> list = printNode(head);
+
+        return help(list, 0, list.size() - 1);
+    }
+
+    public TreeNodes help(List<Integer> list, int left, int right) {
+
+        if (left > right) {
+            return null;
+        }
+
+        int mid = (left + right) / 2;
+
+        TreeNodes root = new TreeNodes(list.get(mid));
+
+        root.left = help(list, left, mid - 1);
+
+        root.right = help(list, mid + 1, right);
+
+        return root;
+
+    }
+
+
+    public ListNode getListNode() {
+
+
+        ListNode node9 = new ListNode(9);
+
+        ListNode node5 = new ListNode(5, node9);
+
+        ListNode node0 = new ListNode(0, node5);
+
+        ListNode node3 = new ListNode(-3, node0);
+
+        ListNode node10 = new ListNode(-10, node3);
+
+
+        return node10;
+    }
+
+
+    public List<Integer> printNode(ListNode listNode) {
+
+        List<Integer> list = new ArrayList<>();
+        while (listNode != null) {
+            System.out.print(listNode.val);
+            System.out.print("  ");
+            list.add(listNode.val);
+            listNode = listNode.next;
+        }
+        return list;
+    }
+
+}
