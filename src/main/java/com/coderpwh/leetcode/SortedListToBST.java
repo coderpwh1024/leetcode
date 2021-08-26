1df46ab0b322dc67fecaa86c365ae7e2530f6a2e
coderpwh <pwhfbc@outlook.com>
2021-08-26 14:22:10 +0800

    Update SortedListToBST.java

--- src/main/java/com/coderpwh/leetcode/SortedListToBST.java
+++ src/main/java/com/coderpwh/leetcode/SortedListToBST.java
@@ -3,6 +3,27 @@ package com.coderpwh.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ *  109 有序链表转换二叉搜索树
+ *  
+ *给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+ *
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * 示例:
+ *
+ * 给定的有序链表： [-10, -3, 0, 5, 9],
+ *
+ * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+ *
+ *       0
+ *      / \
+ *    -3   9
+ *    /   /
+ *  -10  5
+ *
+ *
+ */
 public class SortedListToBST {
 
     public static void main(String[] args) {
@@ -17,7 +38,6 @@ public class SortedListToBST {
     }
 
 
-      
     public TreeNodes sortedListToBST(ListNode head) {
 
         List<Integer> list = printNode(head);
@@ -97,3 +117,4 @@ public class SortedListToBST {
     }
 
 }
+ 
