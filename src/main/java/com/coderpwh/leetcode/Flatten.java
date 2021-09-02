2498d305d76d8abdbdcffed84aeb49e01c93f71b
coderpwh <pwhfbc@outlook.com>
2021-09-02 14:54:04 +0800

    Update Flatten.java

--- src/main/java/com/coderpwh/leetcode/Flatten.java
+++ src/main/java/com/coderpwh/leetcode/Flatten.java
@@ -1,15 +1,26 @@
 package com.coderpwh.leetcode;
 
+import jdk.internal.org.objectweb.asm.util.CheckAnnotationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Flatten {
 
     public static void main(String[] args) {
 
         Flatten flatten = new Flatten();
 
-        TreeNodes root = flatten.createTree();
+//        TreeNodes root = flatten.createTree();
+
+        BiTrees biTrees = flatten.createTree();
+
+        TreeNodes root = biTrees.getRoot();
 
 
         flatten.flatten(root);
+
+//        flatten.preNode(root);
 
     }
 
@@ -18,7 +29,7 @@ public class Flatten {
      *  创建二叉树
      * @return
      */
-    public TreeNodes createTree() {
+    public BiTrees createTree() {
 
         TreeNodes node3 = new TreeNodes(3);
         TreeNodes node4 = new TreeNodes(4);
@@ -32,13 +43,43 @@ public class Flatten {
 
         TreeNodes node1 = new TreeNodes(1, node2, node5);
 
-
-        return node1;
+        return new BiTrees(node1);
     }
 
 
+    // 1  2  3  4 5 6
     public void flatten(TreeNodes root) {
 
+        List<TreeNodes> list = new ArrayList<>();
+
+        preNode(root, list);
+
+        int size = list.size();
+
+
+        for (int i = 1; i < size; i++) {
+
+            TreeNodes prev = list.get(i - 1);
+
+            TreeNodes cur = list.get(i);
+
+            prev.left = null;
+            prev.right = cur;
+            
+        }
+
+
+    }
+
+
+    public void preNode(TreeNodes root, List<TreeNodes> list) {
+
+
+        if (root != null) {
+            list.add(root);
+            preNode(root.left, list);
+            preNode(root.right, list);
+        }
     }
 
 
