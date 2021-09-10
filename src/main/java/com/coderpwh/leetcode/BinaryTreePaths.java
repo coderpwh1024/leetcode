38b9352afe7cbc0dd2659007b9026f3df49ecdff
coderpwh <pwhfbc@outlook.com>
2021-09-10 10:48:54 +0800

    Update BinaryTreePaths.java

--- src/main/java/com/coderpwh/leetcode/BinaryTreePaths.java
+++ src/main/java/com/coderpwh/leetcode/BinaryTreePaths.java
@@ -4,28 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 257. 二叉树的所有路径
+ * <p>
+ * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+ * <p>
+ * 叶子节点 是指没有子节点的节点。
+ * <p>
+ *  
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [1,2,3,null,5]
+ * 输出：["1->2->5","1->3"]
+ * 示例 2：
+ * <p>
+ * 输入：root = [1]
+ * 输出：["1"]
+ *
  * @author coderpwh
  */
 public class BinaryTreePaths {
 
     public static void main(String[] args) {
 
-     BinaryTreePaths path = new BinaryTreePaths();
+        BinaryTreePaths path = new BinaryTreePaths();
 
 
-     // 获取创建二叉树方法
-     BiTrees biTrees = path.create();
+        // 获取创建二叉树方法
+        BiTrees biTrees = path.create();
 
-     // 获取根结点
-     TreeNodes root = biTrees.getRoot();
-
-
-     path.binaryTreePaths(root);
-
-     }
+        // 获取根结点
+        TreeNodes root = biTrees.getRoot();
 
 
-     /***
+        path.binaryTreePaths(root);
+
+    }
+
+
+    /***
      *
      *   创建二叉树
      *
@@ -90,5 +107,3 @@ public class BinaryTreePaths {
 
 
 }
-
-
