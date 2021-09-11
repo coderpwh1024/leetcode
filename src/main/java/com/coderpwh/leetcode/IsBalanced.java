07299a548f87e673037c645e2c3aee200559bf13
coderpwh <pwhfbc@outlook.com>
2021-09-11 15:26:41 +0800

    Update IsBalanced.java

--- src/main/java/com/coderpwh/leetcode/IsBalanced.java
+++ src/main/java/com/coderpwh/leetcode/IsBalanced.java
@@ -4,13 +4,37 @@ public class IsBalanced {
 
     public static void main(String[] args) {
 
+        IsBalanced isBalanced = new IsBalanced();
+
+        BiTrees biTrees = isBalanced.create();
+
+        TreeNodes root = biTrees.getRoot();
+
+
+//        isBalanced.isBalanced(root);
+
+        System.out.println(isBalanced.isBalanced(root));
+
+
     }
 
 
+    public BiTrees create() {
+
+        TreeNodes node7 = new TreeNodes(7);
+
+        TreeNodes node15 = new TreeNodes(15);
+
+        TreeNodes node20 = new TreeNodes(20, node15, node7);
+
+        TreeNodes node9 = new TreeNodes(9);
+
+        TreeNodes node3 = new TreeNodes(3, node9, node20);
+
+        return new BiTrees(node3);
 
 
-
-
+    }
 
 
     public boolean isBalanced(TreeNodes root) {
