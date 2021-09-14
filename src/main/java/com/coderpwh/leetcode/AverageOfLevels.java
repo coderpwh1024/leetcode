e2442a4aa61684b26dde89e6069c4a6402004b59
coderpwh <pwhfbc@outlook.com>
2021-09-14 18:36:02 +0800

    Update AverageOfLevels.java

--- src/main/java/com/coderpwh/leetcode/AverageOfLevels.java
+++ src/main/java/com/coderpwh/leetcode/AverageOfLevels.java
@@ -2,6 +2,26 @@ package com.coderpwh.leetcode;
 
 import java.util.*;
 
+
+/***
+ *   637. 二叉树的层平均值
+ *给定一个非空二叉树, 返回一个由每层节点平均值组成的数组。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 输出：[3, 14.5, 11]
+ * 解释：
+ * 第 0 层的平均值是 3 ,  第1层是 14.5 , 第2层是 11 。因此返回 [3, 14.5, 11] 。
+ 
+ */
 public class AverageOfLevels {
 
     public static void main(String[] args) {
