83428e66fc5d0c1e99b244b67746f1dff84531a9
coderpwh <pwhfbc@outlook.com>
2021-09-29 17:22:13 +0800

    Add files via upload

---
+++ src/main/java/com/coderpwh/leetcode/KthSmallest.java
@@ -0,0 +1,69 @@
+package com.coderpwh.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KthSmallest {
+
+
+    public static void main(String[] args) {
+
+        KthSmallest kthSmallest = new KthSmallest();
+
+        BiTrees biTrees = kthSmallest.create();
+
+        TreeNodes root = biTrees.getRoot();
+
+
+        int k=3;
+
+//        kthSmallest.kthSmallest(root,k);
+
+        System.out.println(kthSmallest.kthSmallest(root,k));
+
+    }
+
+
+    public BiTrees create() {
+
+        TreeNodes node1 = new TreeNodes(1);
+
+        TreeNodes node2 = new TreeNodes(2, node1, null);
+
+        TreeNodes node4 = new TreeNodes(4);
+
+        TreeNodes node3 = new TreeNodes(3, node2, node4);
+
+        TreeNodes node6 = new TreeNodes(6);
+
+        TreeNodes node5 = new TreeNodes(5, node3, node6);
+
+        return new BiTrees(node5);
+    }
+
+    public int kthSmallest(TreeNodes root, int k) {
+
+        List<Integer> list = new ArrayList<>();
+
+        mid(root, list);
+
+        if (list != null) {
+             return  list.get(k-1);
+        }
+
+        return 0;
+    }
+
+
+    public void mid(TreeNodes root, List<Integer> list) {
+
+        if (root != null) {
+            mid(root.left, list);
+            list.add(root.val);
+            mid(root.right, list);
+        }
+
+    }
+
+
+}
