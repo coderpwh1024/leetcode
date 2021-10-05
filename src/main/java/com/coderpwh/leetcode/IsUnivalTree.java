c10c620880221d67e762de0ffc56998be305ad51
coderpwh <pwhfbc@outlook.com>
2021-10-05 15:39:58 +0800

    Add files via upload
    add IsUnivalTreee.java file

---
+++ src/main/java/com/coderpwh/leetcode/IsUnivalTree.java
@@ -0,0 +1,47 @@
+package com.coderpwh.leetcode;
+
+public class IsUnivalTree {
+
+    public static void main(String[] args) {
+
+
+        IsUnivalTree tree = new IsUnivalTree();
+
+        BiTrees biTrees = tree.create();
+
+        TreeNodes root = biTrees.getRoot();
+
+        System.out.println(tree.isUnivalTree(root));
+    }
+
+
+    public BiTrees create() {
+
+        TreeNodes nodes1 = new TreeNodes(2);
+
+        TreeNodes nodes11 = new TreeNodes(1);
+
+        TreeNodes nodes111 = new TreeNodes(1, nodes1, nodes11);
+
+        TreeNodes nodes1111 = new TreeNodes(1);
+
+        TreeNodes nodes11111 = new TreeNodes(1, null, nodes1111);
+
+        TreeNodes root = new TreeNodes(1, nodes111, nodes11111);
+
+
+        return new BiTrees(root);
+
+    }
+
+
+    public boolean isUnivalTree(TreeNodes root) {
+
+        boolean left = (root.left == null || (root.val.equals(root.left.val) && isUnivalTree(root.left)));
+
+        boolean right = (root.right == null || (root.val.equals(root.right.val) && isUnivalTree(root.right)));
+
+        return left && right;
+
+    }
+}
