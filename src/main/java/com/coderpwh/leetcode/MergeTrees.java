52813eb5fd5ce67fa6fb1a50d277b6144609d749
coderpwh <pwhfbc@outlook.com>
2021-10-09 10:40:23 +0800

    Add files via upload
    upload MergeTrees.java file

--- src/main/java/com/coderpwh/leetcode/MergeTrees.java
+++ src/main/java/com/coderpwh/leetcode/MergeTrees.java
@@ -1,7 +1,5 @@
 package com.coderpwh.leetcode;
 
-import javax.management.BadBinaryOpValueExpException;
-
 public class MergeTrees {
 
     public static void main(String[] args) {
@@ -16,7 +14,17 @@ public class MergeTrees {
 
         TreeNodes rootTwo = biTreesTwo.getRoot();
 
-        trees.mergeTrees(rootOne, rootTwo);
+        TreeNodes node = trees.mergeTrees(rootOne, rootTwo);
+
+
+        trees.pre(rootOne);
+        System.out.println();
+        trees.pre(rootTwo);
+
+        System.out.println();
+
+        trees.pre(node);
+
     }
 
 
@@ -24,11 +32,9 @@ public class MergeTrees {
 
         TreeNodes node5 = new TreeNodes(5);
 
-
         TreeNodes node3 = new TreeNodes(3, node5, null);
 
         TreeNodes node2 = new TreeNodes(2);
-
 
         TreeNodes node1 = new TreeNodes(1, node3, node2);
 
@@ -54,9 +60,44 @@ public class MergeTrees {
 
     public TreeNodes mergeTrees(TreeNodes root1, TreeNodes root2) {
 
+        if (root1 == null) {
+            return root2;
+        }
+
+        if (root2 == null) {
+            return root1;
+        }
+
+        TreeNodes root = new TreeNodes(root1.val + root2.val);
+
+        root.left = mergeTrees(root1.left, root2.left);
+
+        root.right = mergeTrees(root1.right, root2.right);
+
+        return root;
+
+    }
 
 
-        return null;
+    public void pre(TreeNodes root) {
+
+        if (root != null) {
+            System.out.print(root.val);
+            System.out.print(" ");
+            pre(root.left);
+            pre(root.right);
+        }
+    }
+
+
+    public void mid(TreeNodes root) {
+
+        if (root != null) {
+            mid(root.left);
+            System.out.print(root.val);
+            System.out.print(" ");
+            mid(root.right);
+        }
     }
 
 
