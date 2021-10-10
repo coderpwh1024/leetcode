31d5eaf7f33cc3f8339e0dcf7edbf3381ce13604
coderpwh <pwhfbc@outlook.com>
2021-10-10 14:42:36 +0800

    Update LargestValues.java

--- src/main/java/com/coderpwh/leetcode/LargestValues.java
+++ src/main/java/com/coderpwh/leetcode/LargestValues.java
@@ -1,4 +1,4 @@
- package com.coderpwh.leetcode;
+package com.coderpwh.leetcode;
 
 import java.util.ArrayList;
 import java.util.Deque;
@@ -18,7 +18,9 @@ public class LargestValues {
 
         TreeNodes root = biTrees.getRoot();
 
-        values.largestValues(root);
+//        values.largestValues(root);
+
+        values.test(root);
 
     }
 
@@ -83,12 +85,60 @@ public class LargestValues {
         }
         return list;
     }
-    
-    
-    
-    
-    
-    
+
+
+    /***
+     *
+     *   思路:
+     *       1.时间复杂度为为O(N^2)
+     *       2.空间复杂度为O(N)
+     *       3.主要是层次遍历，打印每个层次的值
+     * @param root
+     */
+    public void test(TreeNodes root) {
+
+        if (root == null) {
+            return;
+        }
+
+        List<List<Integer>> date = new ArrayList<>();
+
+        Deque<TreeNodes> deque = new LinkedList<>();
+
+        deque.offer(root);
+
+        while (!deque.isEmpty()) {
+
+            int size = deque.size();
+
+
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+
+                TreeNodes node = deque.poll();
+                list.add(node.val);
+
+                if (node.left != null) {
+                    deque.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    deque.offer(node.right);
+                }
+            }
+            date.add(list);
+
+        }
+
+        System.out.println(date.size());
+
+
+        for (List<Integer> li : date) {
+            System.out.println(li);
+        }
+
+
+    }
 
 
 }
