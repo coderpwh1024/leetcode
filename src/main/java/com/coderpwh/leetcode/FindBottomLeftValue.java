9c920303cfab4f894f7e71de1232ca0d2b5170e7
coderpwh <pwhfbc@outlook.com>
2021-10-12 11:38:34 +0800

    Add files via upload
    upload FindBottomLeftValue file

---
+++ src/main/java/com/coderpwh/leetcode/FindBottomLeftValue.java
@@ -0,0 +1,118 @@
+package com.coderpwh.leetcode;
+
+/**
+ * @author coderpwh
+ */
+public class FindBottomLeftValue {
+
+
+    private int value = 0;
+
+    private int Deep = -1;
+
+
+    public static void main(String[] args) {
+
+        FindBottomLeftValue find = new FindBottomLeftValue();
+
+        BiTrees biTree = find.createTwo();
+
+        TreeNodes root = biTree.getRoot();
+
+        find.findBottomLeftValue(root);
+
+    }
+
+
+    public BiTrees create() {
+
+        TreeNodes node7 = new TreeNodes(7);
+
+        TreeNodes node5 = new TreeNodes(5, node7, null);
+
+        TreeNodes node6 = new TreeNodes(6);
+
+        TreeNodes node3 = new TreeNodes(3, node5, node6);
+
+        TreeNodes node4 = new TreeNodes(4);
+
+        TreeNodes node2 = new TreeNodes(2, node4, null);
+
+        TreeNodes root = new TreeNodes(1, node2, node3);
+
+        return new BiTrees(root);
+    }
+
+
+    public BiTrees createTwo() {
+
+        TreeNodes node1 = new TreeNodes(1);
+
+        TreeNodes node3 = new TreeNodes(3);
+
+        TreeNodes node2 = new TreeNodes(2, node1, node3);
+
+        return new BiTrees(node2);
+
+
+    }
+
+
+    public int findBottomLeftValue(TreeNodes root) {
+
+        value = root.val;
+        findValue(root, Deep);
+
+        System.out.println(value);
+        return value;
+    }
+
+
+    public void findValue(TreeNodes root, int deep) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (root.left == null || root.right == null) {
+            if (deep > Deep) {
+                value = root.val;
+                Deep = deep;
+            }
+        }
+
+        if (root.left != null) {
+            findValue(root.left, deep + 1);
+        }
+
+        if (root.right != null) {
+            findValue(root.right, deep + 1);
+        }
+    }
+
+
+    public int mid(TreeNodes root, Integer count) {
+
+        if (root != null) {
+            count = count + 1;
+            mid(root.left, count);
+            return root.val;
+        } else {
+            return -1;
+        }
+    }
+
+
+    public int pre(TreeNodes root, Integer count) {
+
+        if (root != null) {
+            count = count + 1;
+            pre(root.right, count);
+            return root.val;
+        } else {
+            return -1;
+        }
+    }
+
+
+}
