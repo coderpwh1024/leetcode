d089aa9d53830b21f7097c2323f529ca1b2874b2
coderpwh <pwhfbc@outlook.com>
2021-10-13 14:31:12 +0800

    Update ConvertBST.java

--- src/main/java/com/coderpwh/leetcode/ConvertBST.java
+++ src/main/java/com/coderpwh/leetcode/ConvertBST.java
@@ -5,6 +5,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
+ * 538. 把二叉搜索树转换为累加树
+ * <p>
+ * 给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），
+ * 使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和
+ *
  * @author coderpwh
  */
 public class ConvertBST {
