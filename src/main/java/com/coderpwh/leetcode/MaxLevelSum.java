bf53eb2a0145ef80dde43cd36a9ff7c3d50b93e9
coderpwh <pwhfbc@outlook.com>
2021-10-14 11:01:52 +0800

    Update MaxLevelSum.java

--- src/main/java/com/coderpwh/leetcode/MaxLevelSum.java
+++ src/main/java/com/coderpwh/leetcode/MaxLevelSum.java
@@ -6,6 +6,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * 1161. 最大层内元素和
+ * <p>
+ * 给你一个二叉树的根节点 root。设根节点位于二叉树的第 1 层，而根节点的子节点位于第 2 层，依此类推。
+ * <p>
+ * 请你找出层内元素之和 最大 的那几层（可能只有一层）的层号，并返回其中 最小 的那个。
+ * <p>
+ *
  * @author coderpwh
  */
 public class MaxLevelSum {
