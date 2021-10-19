98d02a324871feb17fa0d579acd1349b06d1c15c
coderpwh <pwhfbc@outlook.com>
2021-10-19 11:54:54 +0800

    Update FindTilt.java

--- src/main/java/com/coderpwh/leetcode/FindTilt.java
+++ src/main/java/com/coderpwh/leetcode/FindTilt.java
@@ -1,7 +1,6 @@
  package com.coderpwh.leetcode;
 
 
-
 /**
  * 563 二叉树的坡度
  *
@@ -17,13 +16,13 @@ public class FindTilt {
 
         FindTilt find = new FindTilt();
 
-        BiTrees biTrees = find.create();
+        BiTrees biTrees = find.create2();
 
         TreeNodes root = biTrees.getRoot();
 
+        find.pre(root);
 
         System.out.println(find.findTilt(root));
-
 
     }
 
@@ -45,6 +44,23 @@ public class FindTilt {
         return new BiTrees(node4);
     }
 
+    public BiTrees create2() {
+
+        TreeNodes node1 = new TreeNodes(1);
+        TreeNodes node3 = new TreeNodes(3);
+
+        TreeNodes node2 = new TreeNodes(2, node3, node1);
+
+        TreeNodes node6 = new TreeNodes(6);
+        TreeNodes node5 = new TreeNodes(5);
+
+        TreeNodes node4 = new TreeNodes(4, node6, node5);
+
+        TreeNodes root = new TreeNodes(3, node2, node4);
+
+        return new BiTrees(root);
+    }
+
 
     public int findTilt(TreeNodes root) {
 
@@ -53,6 +69,16 @@ public class FindTilt {
         return total;
     }
 
+
+    /***
+     *  思路:
+     *     1.时间复杂度为O(N)
+     *     2.空间复杂度为O(N)
+     *     3.关键在于求出左右的差的绝对值然后累加起来即可
+     *
+     * @param root
+     * @return
+     */
     public int traverse(TreeNodes root) {
 
         if (root == null) {
@@ -65,7 +91,7 @@ public class FindTilt {
 
         total += Math.abs(left - right);
 
-        return root.val+left+right;
+        return root.val + left + right;
 
     }
 
