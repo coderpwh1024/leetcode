ad466fd4621042f34ca2b1773a047698d9635010
coder-PengWenHao <pengwenhao@aliyun.com>
2022-07-09 15:28:49 +0800

    add List class

---
+++ src/main/java/com/coderpwh/test/SimpleList.java
@@ -0,0 +1,89 @@
+package com.coderpwh.test;
+
+public class SimpleList <T>{
+
+    private  Object testObj;
+
+    private  int size=0;
+
+
+    /***
+     *  获取数组长度
+     * @return
+     */
+    public int size(){
+
+        return  -1;
+    }
+
+
+     // 构造函数中的初始化数组
+    public SimpleList(){
+
+    }
+
+    /***
+     *  判断数组是否为空
+     * @return
+     */
+    public boolean isEmpty(){
+        return  false;
+    }
+
+    /***
+     * 添加新元素
+     * @param e
+     * @return
+     */
+    public boolean add(T e){
+        return  false;
+    }
+
+
+    /***
+     *  把元素e插入到数组index下标的位置
+     * @param e
+     * @param index
+     * @return
+     */
+    public  boolean add(T e,int index){
+        return  false;
+    }
+
+
+    /***
+     *  移除元素
+     * @param o
+     * @return
+     */
+    public  boolean remove(Object o){
+
+        return  false;
+    }
+
+
+    /***
+     *   根据下标移除元素
+     * @param index
+     * @return
+     */
+    public boolean remove(int index){
+
+        return false;
+    }
+
+    /***
+     * 获取其中的元素
+     * @param index
+     * @return
+     */
+    public T get(int index){
+        return  null;
+    }
+
+
+
+
+
+
+}
