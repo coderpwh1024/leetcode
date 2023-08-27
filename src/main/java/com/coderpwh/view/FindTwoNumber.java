0d9c1f0e70738126dfed8064530cd0419b8168b6
coder-PengWenHao <pengwenhao@aliyun.com>
2023-08-27 22:21:14 +0800

    feat:Init FindTwoNumber

---
+++ src/main/java/com/coderpwh/view/FindTwoNumber.java
@@ -0,0 +1,21 @@
+package com.coderpwh.view;
+
+
+import java.util.List;
+
+/***
+ * 1000个数范围是[0,999],有2个相同的数，请设计算法找出来
+ */
+public class FindTwoNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public Integer findNumber(List<Integer> list){
+
+
+        return  -1;
+    }
+
+
+}
