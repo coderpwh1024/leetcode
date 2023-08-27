6c44140bc7c92b8d49da2a3fd36aa8346e03c8dd
coder-PengWenHao <pengwenhao@aliyun.com>
2023-08-27 22:26:39 +0800

    fix:update FindTwoNumber

--- src/main/java/com/coderpwh/view/FindTwoNumber.java
+++ src/main/java/com/coderpwh/view/FindTwoNumber.java
@@ -1,6 +1,7 @@
 package com.coderpwh.view;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 /***
@@ -9,11 +10,34 @@ import java.util.List;
 public class FindTwoNumber {
     public static void main(String[] args) {
 
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        list.add(99);
+        list.add(999);
+        list.add(91);
+        list.add(0);
+        list.add(2);
+        list.add(1);
+
+        FindTwoNumber find = new FindTwoNumber();
+        Integer result = find.findNumber(list);
+        System.out.println(result);
+
+
+
     }
 
     public Integer findNumber(List<Integer> list){
-
-
+         if(list!=null&&list.size()>0){
+             for(int i=0;i<list.size();i++){
+                 for(int j=0;j<list.size();j++){
+                     if(list.get(i).equals(list.get(j))&&i!=j){
+                         return  list.get(i);
+                     }
+                 }
+             }
+         }
         return  -1;
     }
 
