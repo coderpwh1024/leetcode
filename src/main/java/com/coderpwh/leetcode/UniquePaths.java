b2696d45f9fdb3b10680b9391c4f84878e740a11
coderpwh <pwhfbc@outlook.com>
2023-08-30 17:21:20 +0800

    fix:update UniquePaths

---
+++ src/main/java/com/coderpwh/leetcode/UniquePaths.java
@@ -0,0 +1,38 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 62. 不同路径
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * 问总共有多少条不同的路径？
+ *
+ * @author coderpwh
+ * @date 2023/8/30 17:16
+ */
+public class UniquePaths {
+
+
+    public static void main(String[] args) {
+
+        int m = 3;
+        int n = 7;
+
+        UniquePaths paths = new UniquePaths();
+        paths.uniquePaths(m, n);
+
+    }
+
+
+    /***
+     * m:3 n:7 result:28
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+
+
+        return -1;
+    }
+
+}
