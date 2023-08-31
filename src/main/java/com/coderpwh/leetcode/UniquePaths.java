b1fae97d895d2f98c0dece39cdca23084ef2b6d6
coderpwh <pwhfbc@outlook.com>
2023-08-31 15:41:17 +0800

    fix:update UniquePaths

--- src/main/java/com/coderpwh/leetcode/UniquePaths.java
+++ src/main/java/com/coderpwh/leetcode/UniquePaths.java
@@ -14,16 +14,20 @@ public class UniquePaths {
 
     public static void main(String[] args) {
 
-        int m = 3;
-        int n = 7;
-
 //        int m = 3;
-//        int n = 2;
+//        int n = 7;
+
+        int m = 3;
+        int n = 2;
 
         UniquePaths paths = new UniquePaths();
         Integer count = paths.uniquePaths(m, n);
-
         System.out.println(count);
+
+
+        Integer result = paths.getUniquePaths(m, n);
+        System.out.println(result);
+
     }
 
 
@@ -52,5 +56,32 @@ public class UniquePaths {
         }
         return f[m - 1][n - 1];
     }
+
+
+    /***
+     * 动态规范,时间复杂度为O(N^2)
+     * @param m
+     * @param n
+     * @return
+     */
+    public int getUniquePaths(int m, int n) {
+        int[][] f = new int[m][n];
+
+        for (int i = 0; i < m; ++i) {
+            f[i][0] = 1;
+        }
+
+        for (int j = 0; j < n; j++) {
+            f[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+
 
 }
