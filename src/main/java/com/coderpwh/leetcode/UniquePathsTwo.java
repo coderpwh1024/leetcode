929767b7068b19fb6beb260e0eb8a46b37958149
coderpwh <pwhfbc@outlook.com>
2023-09-01 11:15:33 +0800

    fix:update UniquePathsTwo

--- src/main/java/com/coderpwh/leetcode/UniquePathsTwo.java
+++ src/main/java/com/coderpwh/leetcode/UniquePathsTwo.java
@@ -1,6 +1,8 @@
 package com.coderpwh.leetcode;
 
 /**
+ * 63 不同的路径2
+ *
  * @author coderpwh
  * @date 2023/9/1 10:48
  */
@@ -8,8 +10,12 @@ public class UniquePathsTwo {
 
     public static void main(String[] args) {
 
-        int[][] f = new int[3][3];
+        //  1
+//        int[][] f = {{0, 1}, {0, 0}};
 
+
+        // 2
+        int[][] f = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
 
         UniquePathsTwo paths = new UniquePathsTwo();
         Integer result = paths.uniquePathsWithObstacles(f);
@@ -23,8 +29,25 @@ public class UniquePathsTwo {
      * @return
      */
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
 
-        return -1;
+        int[] f = new int[m];
+
+        f[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    f[j] = 0;
+                    continue;
+                }
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    f[j] = f[j] + f[j - 1];
+                }
+            }
+        }
+        return f[m - 1];
     }
 
 }
