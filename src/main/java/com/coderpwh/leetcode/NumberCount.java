c1cc0539fb12e44b7e594dcac4c56cdd21ce4268
coderpwh <pwhfbc@outlook.com>
2023-09-04 14:13:08 +0800

    fix:update NumberCount

--- src/main/java/com/coderpwh/leetcode/NumberCount.java
+++ src/main/java/com/coderpwh/leetcode/NumberCount.java
@@ -1,5 +1,8 @@
 package com.coderpwh.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 357
  *
@@ -25,10 +28,48 @@ public class NumberCount {
      *  n=2:
      *  0<=x<=10^2;
      * 结果为91
+     * 11 22 33 44 55 66 77 88 99
+     *
+     * 10^3
+     * 111 112 ....
+     * 221 222 223
+     *
      * @param n
      * @return
      */
     public int countNumbersWithUniqueDigits(int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        long sum = 1;
+
+        for (int i = 1; i <= n; i++) {
+            sum = sum * 10;
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i < sum; i++) {
+
+
+        }
+
+        return (int) sum;
+    }
+
+
+    /***
+     *  校验数字是否有重复
+     *  99 8
+     *  11 22
+     *  111 221 222 223
+     * @param n
+     * @return
+     */
+    public boolean validNumber(int n) {
+
 
     }
 
