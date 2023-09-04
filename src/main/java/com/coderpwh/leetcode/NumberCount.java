eef5d68df4d738723e59820b12c0087307af40d2
coderpwh <pwhfbc@outlook.com>
2023-09-04 13:57:31 +0800

    feat:add NumberCOunt

---
+++ src/main/java/com/coderpwh/leetcode/NumberCount.java
@@ -0,0 +1,35 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 357
+ *
+ * @author coderpwh
+ * @date 2023/9/4 13:54
+ */
+public class NumberCount {
+
+    public static void main(String[] args) {
+
+        int n = 2;
+
+
+        NumberCount numberCount = new NumberCount();
+        Integer count = numberCount.countNumbersWithUniqueDigits(n);
+
+        System.out.println(count);
+
+    }
+
+
+    /***
+     *  n=2:
+     *  0<=x<=10^2;
+     * 结果为91
+     * @param n
+     * @return
+     */
+    public int countNumbersWithUniqueDigits(int n) {
+
+    }
+
+}
