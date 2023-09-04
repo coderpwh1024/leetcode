10ab1663e2a7c560cb4fa819382660445677d596
coderpwh <pwhfbc@outlook.com>
2023-09-04 15:30:40 +0800

    feat:add IntegerBreak

---
+++ src/main/java/com/coderpwh/leetcode/IntegerBreak.java
@@ -0,0 +1,39 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 343. 整数拆分
+ * <p>
+ * 给定一个正整数 n ，将其拆分为 k 个 正整数 的和（ k >= 2 ），并使这些整数的乘积最大化。
+ * 返回 你可以获得的最大乘积 。
+ * 示例 1:
+ * <p>
+ * 输入: n = 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1。
+ * 示例 2:
+ * <p>
+ * 输入: n = 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+ *
+ * @author coderpwh
+ * @date 2023/9/4 15:28
+ */
+public class IntegerBreak {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+
+        IntegerBreak ib = new IntegerBreak();
+        ib.integerBreak(n);
+
+
+    }
+
+    public int integerBreak(int n) {
+
+        return -1;
+    }
+}
