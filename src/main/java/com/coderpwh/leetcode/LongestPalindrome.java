5b9ead71512dfdfbec2e1792044aeea4f1473509
coderpwh <pwhfbc@outlook.com>
2023-09-05 14:25:52 +0800

    feat: add LongestPalidrome

---
+++ src/main/java/com/coderpwh/leetcode/LongestPalindrome.java
@@ -0,0 +1,43 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 5. 最长回文子串
+ * 提示
+ * 中等
+ * 6.8K
+ * 相关企业
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * <p>
+ * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。l;
+ * 示例 1：
+ * <p>
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ * <p>
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ *
+ * @author coderpwh
+ * @date 2023/9/5 14:23
+ */
+public class LongestPalindrome {
+
+    public static void main(String[] args) {
+
+        String str = "";
+
+        LongestPalindrome palindrome = new LongestPalindrome();
+        String result = palindrome.longestPalindrome(str);
+
+        System.out.println(result);
+    }
+
+    public String longestPalindrome(String s) {
+
+        return "";
+    }
+
+
+}
