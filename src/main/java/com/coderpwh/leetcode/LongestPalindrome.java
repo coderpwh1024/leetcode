dc536e2edffa067bee4cd7a5f12aa80c6c060992
coderpwh <pwhfbc@outlook.com>
2023-09-11 15:44:44 +0800

    feat: add LongestPalindrome

--- src/main/java/com/coderpwh/leetcode/LongestPalindrome.java
+++ src/main/java/com/coderpwh/leetcode/LongestPalindrome.java
@@ -26,7 +26,9 @@ public class LongestPalindrome {
 
     public static void main(String[] args) {
 
-        String str = "";
+//        String str = "babad";
+
+        String str = "cbba";
 
         LongestPalindrome palindrome = new LongestPalindrome();
         String result = palindrome.longestPalindrome(str);
@@ -34,9 +36,52 @@ public class LongestPalindrome {
         System.out.println(result);
     }
 
+
+    /***
+     * 最长回文串
+     * @param s
+     * @return
+     */
     public String longestPalindrome(String s) {
 
-        return "";
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+
+        boolean[][] dp = new boolean[len][len];
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] charArray = s.toCharArray();
+
+        for (int L = 2; L < len; L++) {
+            for (int i = 0; i < len; i++) {
+                int j = L + i - 1;
+                if (j >= len) {
+                    break;
+                }
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j + 1];
+                    }
+                }
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
     }
 
 
