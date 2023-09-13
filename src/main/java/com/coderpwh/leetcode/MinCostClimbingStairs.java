75ef4a4150eb1e3beceb5856970598b87b6bac05
coderpwh <pwhfbc@outlook.com>
2023-09-13 11:29:26 +0800

    feat:MinCostClimbingStairs

---
+++ src/main/java/com/coderpwh/leetcode/MinCostClimbingStairs.java
@@ -0,0 +1,26 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 746. 使用最小花费爬楼梯
+ *
+ * @author coderpwh
+ * @date 2023/9/13 11:27
+ */
+public class MinCostClimbingStairs {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    /***
+     * 最小花费爬楼梯
+     * @param cost
+     * @return
+     */
+    public int minCostClimbingStairs(int[] cost) {
+
+    }
+
+
+}
