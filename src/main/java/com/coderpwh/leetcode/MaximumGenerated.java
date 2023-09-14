eae168cf66b504e97a2af3b7f8ebc52c25952d44
coderpwh <pwhfbc@outlook.com>
2023-09-14 16:47:00 +0800

    fix:update MaximumGenerated

--- src/main/java/com/coderpwh/leetcode/MaximumGenerated.java
+++ src/main/java/com/coderpwh/leetcode/MaximumGenerated.java
@@ -24,19 +24,23 @@ public class MaximumGenerated {
      * 输入：n = 7
      * 输出：3
      * 解释：根据规则：
-     *   nums[0] = 0
-     *   nums[1] = 1
-     *   nums[(1 * 2) = 2] = nums[1] = 1
-     *   nums[(1 * 2) + 1 = 3] = nums[1] + nums[2] = 1 + 1 = 2
-     *   nums[(2 * 2) = 4] = nums[2] = 1
-     *   nums[(2 * 2) + 1 = 5] = nums[2] + nums[3] = 1 + 2 = 3
-     *   nums[(3 * 2) = 6] = nums[3] = 2
-     *   nums[(3 * 2) + 1 = 7] = nums[3] + nums[4] = 2 + 1 = 3
+     * nums[0] = 0
+     * nums[1] = 1
+     * nums[(1 * 2) = 2] = nums[1] = 1
+     * nums[(1 * 2) + 1 = 3] = nums[1] + nums[2] = 1 + 1 = 2
+     * nums[(2 * 2) = 4] = nums[2] = 1
+     * nums[(2 * 2) + 1 = 5] = nums[2] + nums[3] = 1 + 2 = 3
+     * nums[(3 * 2) = 6] = nums[3] = 2
+     * nums[(3 * 2) + 1 = 7] = nums[3] + nums[4] = 2 + 1 = 3
      * 因此，nums = [0,1,1,2,1,3,2,3]，最大值 3
+     *
      * @param n
      * @return
      */
     public int getMaximumGenerated(int n) {
+
+        int[] arr = {0, 0, 1, 2, 1, 3, 2, 3};
+
 
         return -1;
     }
