2a4c71d89398fc6819647e8343c5f0b662c65e37
coderpwh <pwhfbc@outlook.com>
2023-09-14 14:06:06 +0800

    fix:update MinCostClimbingStairs

--- src/main/java/com/coderpwh/leetcode/MinCostClimbingStairs.java
+++ src/main/java/com/coderpwh/leetcode/MinCostClimbingStairs.java
@@ -2,6 +2,11 @@ package com.coderpwh.leetcode;
 
 /**
  * 746. 使用最小花费爬楼梯
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ * <p>
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ * <p>
+ * 请你计算并返回达到楼梯顶部的最低花费。
  *
  * @author coderpwh
  * @date 2023/9/13 11:27
@@ -10,8 +15,14 @@ public class MinCostClimbingStairs {
 
     public static void main(String[] args) {
 
-    }
+        // 6
+        int arr[] = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
 
+        MinCostClimbingStairs minCost = new MinCostClimbingStairs();
+        Integer result = minCost.minCostClimbingStairs(arr);
+        System.out.println(result);
+
+    }
 
     /***
      * 最小花费爬楼梯
@@ -20,6 +31,17 @@ public class MinCostClimbingStairs {
      */
     public int minCostClimbingStairs(int[] cost) {
 
+        int sum = 0;
+
+        for (int i = 0; i < cost.length; i++) {
+            for (int j = 0; j < cost.length; j++) {
+                if (cost[i] < cost[j]) {
+                    sum += cost[i];
+                }
+            }
+        }
+
+        return -1;
     }
 
 
