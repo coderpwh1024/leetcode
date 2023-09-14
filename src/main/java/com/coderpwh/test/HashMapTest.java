07b101a109cbddf38db5fdff49a636265fcd1968
coderpwh <pwhfbc@outlook.com>
2023-09-14 15:46:39 +0800

    fix:update HashMapTest

--- src/main/java/com/coderpwh/test/HashMapTest.java
+++ src/main/java/com/coderpwh/test/HashMapTest.java
@@ -8,8 +8,31 @@ import java.util.HashMap;
  */
 public class HashMapTest {
 
+
+    private static HashMap<Integer, String> map = new HashMap<>(2, 0.75f);
+
     public static void main(String[] args) {
-        HashMap map = new HashMap();
+        map.put(5, "C");
+
+        new Thread("Thread1") {
+            @Override
+            public void run() {
+                map.put(7, "B");
+                System.out.println(map);
+            }
+        }.start();
+
+        new Thread("Thread2") {
+            @Override
+            public void run() {
+                map.put(3, "A");
+                System.out.println(map);
+            }
+
+            ;
+        }.start();
+
+
     }
 
 }
