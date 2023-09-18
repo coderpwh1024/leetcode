83331b625ccd9e8fbc4fbc9ec345198dab17f6a7
coderpwh <pwhfbc@outlook.com>
2023-09-18 11:24:00 +0800

    fix:update DeletedNode2

---
+++ src/main/java/com/coderpwh/leetcode/DeleteNode2.java
@@ -0,0 +1,39 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 剑指 Offer 18. 删除链表的节点
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+ * <p>
+ * 返回删除后的链表的头节点。
+ * <p>
+ * 注意：此题对比原题有改动
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: head = [4,5,1,9], val = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 示例 2:
+ * <p>
+ * 输入: head = [4,5,1,9], val = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *
+ * @author coderpwh
+ * @date 2023/9/18 11:22
+ */
+public class DeleteNode2 {
+
+    public static void main(String[] args) {
+
+        DeleteNode2 delete = new DeleteNode2();
+        delete.deleteNode();
+    }
+
+    public ListNode deleteNode(ListNode head, int val) {
+
+        return null;
+    }
+
+
+}
