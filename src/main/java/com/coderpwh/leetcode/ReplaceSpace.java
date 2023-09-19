8275b8b83b98b24eec774fe25c140bcd55258230
coder-PengWenHao <pengwenhao@aliyun.com>
2023-09-19 21:49:28 +0800

    feat: Init ReplaceSpace

---
+++ src/main/java/com/coderpwh/leetcode/ReplaceSpace.java
@@ -0,0 +1,19 @@
+package com.coderpwh.leetcode;
+
+
+/***
+ * 剑指 Offer 05. 替换空格
+ *请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ *
+ */
+public class ReplaceSpace {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String replaceSpace(String s) {
+
+    }
+
+}
