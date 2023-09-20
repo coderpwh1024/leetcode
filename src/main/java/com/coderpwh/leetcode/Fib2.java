92e4771728612ea948c278397349459e1afd1c42
coderpwh <pwhfbc@outlook.com>
2023-09-20 16:21:32 +0800

    feat:Fib2

---
+++ src/main/java/com/coderpwh/leetcode/Fib2.java
@@ -0,0 +1,50 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 剑指 Offer 10- I. 斐波那契数列
+ * <p>
+ * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+ * <p>
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ * <p>
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 2
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * 输入：n = 5
+ * 输出：5
+ *
+ * @author coderpwh
+ * @date 2023/9/20 16:11
+ */
+public class Fib2 {
+
+
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        Fib2 fib = new Fib2();
+
+        Integer result = fib.fib(n);
+        System.out.println(result);
+    }
+
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
