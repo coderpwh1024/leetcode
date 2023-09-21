b5aa1066a1810045e90f2f8bb4d7104477c78b0a
coderpwh <pwhfbc@outlook.com>
2023-09-21 10:46:56 +0800

    feat:Init GetKthFromEnd

---
+++ src/main/java/com/coderpwh/leetcode/GetKthFromEnd.java
@@ -0,0 +1,54 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ * 简单
+ * 506
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+ * <p>
+ * 返回链表 4->5.
+ *
+ * @author coderpwh
+ * @date 2023/9/21 10:40
+ */
+public class GetKthFromEnd {
+
+    public static void main(String[] args) {
+
+        int k = 2;
+
+        GetKthFromEnd end = new GetKthFromEnd();
+        end.getKthFromEnd();
+
+    }
+
+
+    /***
+     * 创建链表
+     * @return
+     */
+    public ListNode createNode() {
+
+        ListNode node = new ListNode();
+
+        return node;
+    }
+
+
+    /***
+     * 获取倒数第k个节点
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode getKthFromEnd(ListNode head, int k) {
+
+        return null;
+    }
+
+}
