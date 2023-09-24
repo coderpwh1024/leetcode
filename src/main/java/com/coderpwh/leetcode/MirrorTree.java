040b22d4a3f12558f101c669d9999c21e562598d
coder-PengWenHao <pengwenhao@aliyun.com>
2023-09-24 16:41:27 +0800

    feat:Init MirrorTree

---
+++ src/main/java/com/coderpwh/leetcode/MirrorTree.java
@@ -0,0 +1,24 @@
+package com.coderpwh.leetcode;
+
+
+/***
+ * LCR 144. 翻转二叉树
+ *
+ *给定一棵二叉树的根节点 root，请左右翻转这棵二叉树，并返回其根节点。
+ *
+ * 输入：root = [5,7,9,8,3,2,4]
+ * 输出：[5,9,7,4,2,3,8]
+ */
+public class MirrorTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode mirrorTree(TreeNode root) {
+
+        return  null;
+    }
+
+
+}
