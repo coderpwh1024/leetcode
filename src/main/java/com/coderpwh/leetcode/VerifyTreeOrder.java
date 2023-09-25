ef0074c225e5da3b925016c42375e9f76ba8d5d8
coderpwh <pwhfbc@outlook.com>
2023-09-25 16:52:12 +0800

    feat:Init verifyTreeOrder

---
+++ src/main/java/com/coderpwh/leetcode/VerifyTreeOrder.java
@@ -0,0 +1,23 @@
+package com.coderpwh.leetcode;
+
+/**
+ * LCR 152. 验证二叉搜索树的后序遍历序列
+ * 请实现一个函数来判断整数数组 postorder 是否为二叉搜索树的后序遍历结果。
+ *
+ * @author coderpwh
+ * @date 2023/9/25 16:50
+ */
+public class VerifyTreeOrder {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean verifyTreeOrder(int[] postorder) {
+
+        return false;
+    }
+
+
+}
