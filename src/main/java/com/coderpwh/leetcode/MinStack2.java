5f974994d6b2392b74441471ed7909d460c75c99
coder-PengWenHao <pengwenhao@aliyun.com>
2023-09-30 09:11:00 +0800

    feat:Init MinStack2

---
+++ src/main/java/com/coderpwh/leetcode/MinStack2.java
@@ -0,0 +1,46 @@
+package com.coderpwh.leetcode;
+
+
+/***
+ *LCR 147. 最小栈
+ *
+ * 请你设计一个 最小栈 。它提供 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ *
+ *
+ *
+ * 实现 MinStack 类:
+ *
+ * MinStack() 初始化堆栈对象。
+ * void push(int val) 将元素val推入堆栈。
+ * void pop() 删除堆栈顶部的元素。
+ * int top() 获取堆栈顶部的元素。
+ * int getMin() 获取堆栈中的最小元素。
+ */
+public class MinStack2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public MinStack2() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+
+
+}
