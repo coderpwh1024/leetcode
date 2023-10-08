b534cafc3a39676993254c1275f1c7c6b3801646
coderpwh <pwhfbc@outlook.com>
2023-10-08 09:59:18 +0800

    feat:Init InventoryManagementThree

---
+++ src/main/java/com/coderpwh/leetcode/InventoryManagementThree.java
@@ -0,0 +1,38 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 仓库管理员以数组 stock 形式记录商品库存表，其中 stock[i]
+ * 表示对应商品库存余量。请返回库存余量最少的 cnt 个商品余量，返回 顺序不限。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：stock = [2,5,7,4], cnt = 1
+ * 输出：[2]
+ * 示例 2：
+ * <p>
+ * 输入：stock = [0,2,3,6], cnt = 2
+ * 输出：[0,2] 或 [2,0]
+ *
+ * @author coderpwh
+ * @date 2023/9/26 16:21
+ */
+public class InventoryManagementThree {
+
+    public static void main(String[] args) {
+
+        int[] stock = {2, 5, 7, 4};
+
+        int cnt = 1;
+
+        InventoryManagementThree management = new InventoryManagementThree();
+//        management.inventoryManagement();
+
+    }
+
+    public int[] inventoryManagement(int[] stock, int cnt) {
+
+        return null;
+    }
+
+
+}
