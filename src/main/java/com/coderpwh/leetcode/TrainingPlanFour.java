5403572fdf961104301440c0c6176677866b760b
coderpwh <pwhfbc@outlook.com>
2023-10-10 11:04:23 +0800

    fix:update TrainingPlanFour

--- src/main/java/com/coderpwh/leetcode/TrainingPlanFour.java
+++ src/main/java/com/coderpwh/leetcode/TrainingPlanFour.java
@@ -1,5 +1,7 @@
 package com.coderpwh.leetcode;
 
+import java.util.HashMap;
+
 /**
  * LCR 178. 训练计划 VI
  * 教学过程中，教练示范一次，学员跟做三次。该过程被混乱剪辑后，记录于数组 actions，
@@ -21,7 +23,9 @@ public class TrainingPlanFour {
 
     public static void main(String[] args) {
 
-        int actions[] = {12, 1, 6, 12, 6, 12, 6};
+//        int actions[] = {12, 1, 6, 12, 6, 12, 6};
+
+        int actions[] = {5, 7, 5, 5};
 
         TrainingPlanFour plan = new TrainingPlanFour();
         Integer result = plan.trainingPlan(actions);
@@ -30,7 +34,22 @@ public class TrainingPlanFour {
 
 
     public int trainingPlan(int[] actions) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for (int i = 0; i < actions.length; i++) {
+            if (map.containsKey(actions[i])) {
+                Integer count = map.get(actions[i]) + 1;
+                map.put(actions[i], count);
+            } else {
+                map.put(actions[i], 1);
+            }
+        }
+
+        for (Integer key : map.keySet()) {
+            if (map.get(key) <= 1) {
+                return key;
+            }
+        }
         return -1;
     }
 
