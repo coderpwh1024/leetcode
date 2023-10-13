c76189d1c8f0e7a27c3fc4690f48216c80194ec0
coderpwh <pwhfbc@outlook.com>
2023-10-13 09:52:14 +0800

    feat:Init RemoveDuplicateNodes

---
+++ src/main/java/com/coderpwh/leetcode/RemoveDuplicateNodes.java
@@ -0,0 +1,35 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 面试题 02.01. 移除重复节点
+ * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入：[1, 2, 3, 3, 2, 1]
+ * 输出：[1, 2, 3]
+ * 示例2:
+ * <p>
+ * 输入：[1, 1, 1, 1, 2]
+ * 输出：[1, 2]
+ * 提示：
+ * <p>
+ * 链表长度在[0, 20000]范围内。
+ * 链表元素在[0, 20000]范围内。
+ *
+ * @author coderpwh
+ * @date 2023/10/13 9:50
+ */
+public class RemoveDuplicateNodes {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode removeDuplicateNodes(ListNode head) {
+
+        return null;
+    }
+
+
+}
