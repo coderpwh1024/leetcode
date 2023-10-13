28933af05c3a388d6fc836725c8b5614ca94abf0
coderpwh <pwhfbc@outlook.com>
2023-10-13 13:57:33 +0800

    feat:Init AddTwoNumbersTwo

---
+++ src/main/java/com/coderpwh/leetcode/AddTwoNumbersTwo.java
@@ -0,0 +1,33 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 面试题 02.05. 链表求和
+ * <p>
+ * 给定两个用链表表示的整数，每个节点包含一个数位。
+ * <p>
+ * 这些数位是反向存放的，也就是个位排在链表首部。
+ * <p>
+ * 编写函数对这两个整数求和，并用链表形式返回结果。
+ * 示例：
+ * <p>
+ * 输入：(6 -> 1 -> 7) + (2 -> 9 -> 5)，即617 + 295
+ * 输出：9 -> 1 -> 2，即912
+ *
+ * @author coderpwh
+ * @date 2023/10/13 13:50
+ */
+public class AddTwoNumbersTwo {
+
+    public static void main(String[] args) {
+
+        AddTwoNumbersTwo numbers = new AddTwoNumbersTwo();
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        return l1;
+    }
+
+
+}
