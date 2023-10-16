d5cc0fadd3008cfbe2390f8e184bb3d4053522f6
coderpwh <pwhfbc@outlook.com>
2023-10-16 11:41:35 +0800

    fix:update partition

--- src/main/java/com/coderpwh/leetcode/Partition.java
+++ src/main/java/com/coderpwh/leetcode/Partition.java
@@ -1,6 +1,22 @@
 package com.coderpwh.leetcode;
 
 /**
+ * 面试题 02.04. 分割链表
+ * <p>
+ * 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+ * 你不需要 保留 每个分区中各节点的初始相对位置。
+ *
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,4,3,2,5,2], x = 3
+ * 输出：[1,2,2,4,3,5]
+ * 示例 2：
+ * <p>
+ * 输入：head = [2,1], x = 2
+ * 输出：[1,2]
+ *
  * @author coderpwh
  * @date 2023/10/16 10:37
  */
@@ -38,8 +54,35 @@ public class Partition {
     }
 
 
+    /***
+     * 删除
+     * @param head
+     * @param x
+     * @return
+     */
     public ListNode partition(ListNode head, int x) {
 
+        ListNode small = new ListNode(0);
+        ListNode smallHead = small;
+
+        ListNode large = new ListNode(0);
+        ListNode largeHead = large;
+
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+
+        large.next = null;
+        small.next = largeHead.next;
+
+        return smallHead.next;
     }
 
 
