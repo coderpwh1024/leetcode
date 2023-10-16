718901b10d68d94d28ccbfb90a0bb9625b84c81b
coderpwh <pwhfbc@outlook.com>
2023-10-16 11:23:05 +0800

    fix:update Partition

--- src/main/java/com/coderpwh/leetcode/Partition.java
+++ src/main/java/com/coderpwh/leetcode/Partition.java
@@ -8,9 +8,24 @@ public class Partition {
 
     public static void main(String[] args) {
 
+        Partition partition = new Partition();
+
+        // 创建链表
+        ListNode head = partition.createNode();
+
+        int x = 3;
+
+        ListNode root = partition.partition(head, x);
+
+
     }
 
 
+    /***
+     * 创建链表
+     *
+     * @return
+     */
     public ListNode createNode() {
         ListNode node22 = new ListNode(2);
         ListNode node5 = new ListNode(5, node22);
@@ -18,9 +33,7 @@ public class Partition {
         ListNode node3 = new ListNode(3, node2);
 
         ListNode node4 = new ListNode(4, node3);
-
         ListNode root = new ListNode(1, node4);
-
         return root;
     }
 
