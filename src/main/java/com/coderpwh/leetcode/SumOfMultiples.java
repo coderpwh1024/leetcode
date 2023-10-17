aac628b59a4f382b64a3c76b9e344a4de31558d4
coderpwh <pwhfbc@outlook.com>
2023-10-17 10:30:27 +0800

    fix:update SumofMultiples

--- src/main/java/com/coderpwh/leetcode/SumOfMultiples.java
+++ src/main/java/com/coderpwh/leetcode/SumOfMultiples.java
@@ -41,6 +41,18 @@ public class SumOfMultiples {
 
     public int sumOfMultiples(int n) {
 
+        Integer sum = 0;
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 3 == 0) {
+                sum += i;
+            } else if (i % 5 == 0) {
+                sum += i;
+            } else if (i % 7 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 
 
