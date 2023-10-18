383a9e33ed6b438c22ee871c31219ac3225fceaf
coderpwh <pwhfbc@outlook.com>
2023-10-18 13:39:59 +0800

    feat:Init update InorderSuccessOr

---
+++ src/main/java/com/coderpwh/leetcode/InorderSuccessor.java
@@ -0,0 +1,44 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 设计一个算法，找出二叉搜索树中指定节点的“下一个”节点（也即中序后继）。
+ * <p>
+ * 如果指定节点没有对应的“下一个”节点，则返回null。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [2,1,3], p = 1
+ * <p>
+ * 2
+ * / \
+ * 1   3
+ * <p>
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,null,1], p = 6
+ * <p>
+ * 5
+ * / \
+ * 3   6
+ * / \
+ * 2   4
+ * /
+ * 1
+ * <p>
+ * 输出: null
+ *
+ * @author coderpwh
+ * @date 2023/10/18 13:37
+ */
+public class InorderSuccessor {
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        return null;
+    }
+
+}
