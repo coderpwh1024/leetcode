a77c34df994ee3206677d58aad074b2cae4fd18c
coderpwh <pwhfbc@outlook.com>
2023-10-18 11:04:42 +0800

    fix:update IsValidBSTTwo

--- src/main/java/com/coderpwh/leetcode/IsValidBSTTwo.java
+++ src/main/java/com/coderpwh/leetcode/IsValidBSTTwo.java
@@ -1,5 +1,10 @@
 package com.coderpwh.leetcode;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 面试题 04.05. 合法二叉搜索树
  * 实现一个函数，检查一棵二叉树是否为二叉搜索树。
@@ -28,12 +33,55 @@ public class IsValidBSTTwo {
 
     public static void main(String[] args) {
 
+        IsValidBSTTwo valid = new IsValidBSTTwo();
+
+        // 创建二叉树
+        TreeNode node = valid.createNode();
+
+        Boolean result = valid.isValidBST(node);
+        System.out.println(result);
+
     }
 
 
+    /***
+     * 创建二叉树
+     * @return
+     */
+    public TreeNode createNode() {
+        TreeNode node3 = new TreeNode("3");
+        TreeNode node6 = new TreeNode("6");
+
+        TreeNode node1 = new TreeNode("1", node3, node6);
+        TreeNode node4 = new TreeNode("4");
+
+        TreeNode root = new TreeNode("5", node1, node4);
+        return root;
+    }
 
     public boolean isValidBST(TreeNode root) {
 
+        List<Integer> list = new ArrayList<>();
+
+        mid(root, list);
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
+
+    public void mid(TreeNode root, List<Integer> list) {
+
+        if (root != null) {
+            mid(root.left, list);
+            list.add(Integer.valueOf(root.val));
+            mid(root.right, list);
+        }
+
+    }
+
 
 }
