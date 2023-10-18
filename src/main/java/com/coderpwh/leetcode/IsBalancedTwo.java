a18ebd4bea322e9e046eda7e657cc99d84a530dd
coderpwh <pwhfbc@outlook.com>
2023-10-18 10:08:46 +0800

    fix:update IsBalancedTwo

---
+++ src/main/java/com/coderpwh/leetcode/IsBalancedTwo.java
@@ -0,0 +1,42 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 面试题 04.04. 检查平衡性
+ * <p>
+ * 实现一个函数，检查二叉树是否平衡。在这个问题中，平衡树的定义如下：任意一个节点，其两棵子树的高度差不超过 1。
+ * <p>
+ * <p>
+ * 示例 1:
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回 true 。
+ * 示例 2:
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ * 1
+ * / \
+ * 2   2
+ * / \
+ * 3   3
+ * / \
+ * 4   4
+ * 返回 false 。
+ *
+ * @author coderpwh
+ * @date 2023/10/18 9:55
+ */
+public class IsBalancedTwo {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean isBalanced(TreeNode root) {
+        return false;
+    }
+
+}
