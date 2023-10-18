5dc50d95ce3cf8e37faacc92931ed13c570b9513
coderpwh <pwhfbc@outlook.com>
2023-10-18 10:56:21 +0800

    feat:Init IsValidBSTTwo

---
+++ src/main/java/com/coderpwh/leetcode/IsValidBSTTwo.java
@@ -0,0 +1,39 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 面试题 04.05. 合法二叉搜索树
+ * 实现一个函数，检查一棵二叉树是否为二叉搜索树。
+ * <p>
+ * 示例 1:
+ * 输入:
+ * 2
+ * / \
+ * 1   3
+ * 输出: true
+ * 示例 2:
+ * 输入:
+ * 5
+ * / \
+ * 1   4
+ * / \
+ * 3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ * 根节点的值为 5 ，但是其右子节点值为 4 。
+ *
+ * @author coderpwh
+ * @date 2023/10/18 10:54
+ */
+public class IsValidBSTTwo {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public boolean isValidBST(TreeNode root) {
+
+    }
+
+}
