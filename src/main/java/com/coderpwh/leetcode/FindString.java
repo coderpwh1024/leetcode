776f71674f8baef17805690feb69c854cd3302b1
coder-PengWenHao <pengwenhao@aliyun.com>
2023-10-22 14:26:03 +0800

    fix:update FindString

--- src/main/java/com/coderpwh/leetcode/FindString.java
+++ src/main/java/com/coderpwh/leetcode/FindString.java
@@ -31,11 +31,23 @@ public class FindString {
 
         FindString findString = new FindString();
 
+         int count = findString.findString(arr,str);
+        System.out.println(count);
+
     }
 
     public int findString(String[] words, String s) {
 
 
+        if(words==null||words.length<=0){
+            return  -1;
+        }
+
+        for(int i=0;i<words.length;i++){
+            if(s.equals(words[i])){
+                return  i;
+            }
+        }
         return -1;
     }
 }
