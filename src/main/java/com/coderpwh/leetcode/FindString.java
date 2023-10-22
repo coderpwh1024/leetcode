9c06536ef500a853773a8f24f1a68755942be118
coder-PengWenHao <pengwenhao@aliyun.com>
2023-10-22 14:22:52 +0800

    fix:update FindString

--- src/main/java/com/coderpwh/leetcode/FindString.java
+++ src/main/java/com/coderpwh/leetcode/FindString.java
@@ -1,7 +1,41 @@
 package com.coderpwh.leetcode;
 
+
+/***
+ * 面试题 10.05. 稀疏数组搜索
+ * 提示
+ * 简单
+ * 84
+ * 相关企业
+ * 稀疏数组搜索。有个排好序的字符串数组，其中散布着一些空字符串，编写一种方法，找出给定字符串的位置。
+ *
+ * 示例1:
+ *
+ *  输入: words = ["at", "", "", "", "ball", "", "", "car", "", "","dad", "", ""], s = "ta"
+ *  输出：-1
+ *  说明: 不存在返回-1。
+ * 示例2:
+ *
+ *  输入：words = ["at", "", "", "", "ball", "", "", "car", "", "","dad", "", ""], s = "ball"
+ *  输出：4
+ * 提示:
+ *
+ * words的长度在[1, 1000000]之间
+ */
 public class FindString {
     public static void main(String[] args) {
 
+        String arr[] = {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""};
+        String str = "ball";
+
+
+        FindString findString = new FindString();
+
+    }
+
+    public int findString(String[] words, String s) {
+
+
+        return -1;
     }
 }
