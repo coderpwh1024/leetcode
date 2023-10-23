976ab59f69ff6c0d92e8462287059a7c060a72fa
coderpwh <pwhfbc@outlook.com>
2023-10-23 13:42:13 +0800

    feat:Init Multiply

---
+++ src/main/java/com/coderpwh/leetcode/Multiply.java
@@ -0,0 +1,33 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 递归乘法。 写一个递归函数，不使用 * 运算符， 实现两个正整数的相乘。可以使用加号、减号、位移，但要吝啬一些。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入：A = 1, B = 10
+ * 输出：10
+ * 示例2:
+ * <p>
+ * 输入：A = 3, B = 4
+ * 输出：12
+ * 提示:
+ * <p>
+ * 保证乘法范围不会溢出
+ *
+ * @author coderpwh
+ * @date 2023/10/23 13:39
+ */
+public class Multiply {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int multiply(int A, int B) {
+
+        return -1;
+    }
+
+
+}
