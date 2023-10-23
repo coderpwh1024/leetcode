1f4d81ce6c2cb31393919cba46fcc07bd40b78ae
coderpwh <pwhfbc@outlook.com>
2023-10-23 16:35:30 +0800

    fix:update WaysToChange

--- src/main/java/com/coderpwh/leetcode/WaysToChange.java
+++ src/main/java/com/coderpwh/leetcode/WaysToChange.java
@@ -3,7 +3,8 @@ package com.coderpwh.leetcode;
 /**
  * 面试题 08.11. 硬币
  * <p>
- * 硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
+ * 硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，
+ * 编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
  * <p>
  * 示例1:
  * <p>
@@ -36,12 +37,25 @@ public class WaysToChange {
 
 
     public static void main(String[] args) {
+        int n =11;
 
-        WaysToChange waysToChange = new WaysToChange();
+        WaysToChange change = new WaysToChange();
+        int result = change.waysToChange(n);
+        System.out.println(result);
+
     }
 
     public int waysToChange(int n) {
-        return -1;
+        int[] nums = {25, 10, 5, 1};
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int i = num; i <= n; i++) {
+                dp[i] = dp[i - num] + dp[i];
+            }
+        }
+        return (int) (dp[n] % 1000000007);
     }
 
 
