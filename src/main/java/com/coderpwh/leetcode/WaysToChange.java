fdd1548e203359d1dbd483f4d7b0b88fa7f005bf
coderpwh <pwhfbc@outlook.com>
2023-10-23 16:11:12 +0800

    feat:Init WaysToChange

---
+++ src/main/java/com/coderpwh/leetcode/WaysToChange.java
@@ -0,0 +1,48 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 面试题 08.11. 硬币
+ * <p>
+ * 硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: n = 5
+ * 输出：2
+ * 解释: 有两种方式可以凑成总金额:
+ * 5=5
+ * 5=1+1+1+1+1
+ * 示例2:
+ * <p>
+ * 输入: n = 10
+ * 输出：4
+ * 解释: 有四种方式可以凑成总金额:
+ * 10=10
+ * 10=5+5
+ * 10=5+1+1+1+1+1
+ * 10=1+1+1+1+1+1+1+1+1+1
+ * 说明：
+ * <p>
+ * 注意:
+ * <p>
+ * 你可以假设：
+ * <p>
+ * 0 <= n (总金额) <= 1000000
+ *
+ * @author coderpwh
+ * @date 2023/10/23 16:09
+ */
+public class WaysToChange {
+
+
+    public static void main(String[] args) {
+
+        WaysToChange waysToChange = new WaysToChange();
+    }
+
+    public int waysToChange(int n) {
+        return -1;
+    }
+
+
+}
