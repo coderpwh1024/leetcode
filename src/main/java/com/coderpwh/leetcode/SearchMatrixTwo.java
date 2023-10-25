bac05cdd0435456d08f2d74123959e818787acc9
coderpwh <pwhfbc@outlook.com>
2023-10-25 10:02:20 +0800

    fix:update SearchMatrixTwo

--- src/main/java/com/coderpwh/leetcode/SearchMatrixTwo.java
+++ src/main/java/com/coderpwh/leetcode/SearchMatrixTwo.java
@@ -42,7 +42,15 @@ public class SearchMatrixTwo {
 
 
     public boolean searchMatrix(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
 
+        return false;
     }
 
 }
