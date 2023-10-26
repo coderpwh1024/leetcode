2e1b9aae558c574e7ee8c06c644936f679590b7b
coderpwh <pwhfbc@outlook.com>
2023-10-26 16:44:09 +0800

    feat:Init RightSideView

---
+++ src/main/java/com/coderpwh/leetcode/RightSideView.java
@@ -0,0 +1,40 @@
+package com.coderpwh.leetcode;
+
+import java.util.List;
+
+/**
+ * 199. 二叉树的右视图
+ * <p>
+ * 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * <p>
+ * <p>
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1,3,4]
+ * 示例 2:
+ * <p>
+ * 输入: [1,null,3]
+ * 输出: [1,3]
+ * 示例 3:
+ * <p>
+ * 输入: []
+ * 输出: []
+ *
+ * @author coderpwh
+ * @date 2023/10/26 16:41
+ */
+public class RightSideView {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+
+        return  null;
+    }
+
+}
