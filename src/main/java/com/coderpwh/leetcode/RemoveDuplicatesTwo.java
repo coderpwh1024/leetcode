c3829285197580e09dffd2a4fb83d3f5b5ee6662
coderpwh <pwhfbc@outlook.com>
2023-10-26 10:48:32 +0800

    fix:update RemoveDuplicatesTwo

--- src/main/java/com/coderpwh/leetcode/RemoveDuplicatesTwo.java
+++ src/main/java/com/coderpwh/leetcode/RemoveDuplicatesTwo.java
@@ -1,5 +1,8 @@
 package com.coderpwh.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 26. 删除有序数组中的重复项
  * <p>
@@ -24,11 +27,28 @@ public class RemoveDuplicatesTwo {
         int arr[] = {1, 1, 2};
 
         RemoveDuplicatesTwo removeDuplicatesTwo = new RemoveDuplicatesTwo();
-        removeDuplicatesTwo.removeDuplicates(arr);
+        Integer result = removeDuplicatesTwo.removeDuplicates(arr);
+        System.out.println(result);
     }
 
     public int removeDuplicates(int[] nums) {
 
+        if (nums == null || nums.length <= 0) {
+            return 0;
+        }
+
+        int p = 0;
+        int q = 1;
+
+        while (q < nums.length) {
+
+            if (nums[p] != nums[q]) {
+                nums[p + 1] = nums[q];
+                p++;
+            }
+            q++;
+        }
+        return p + 1;
     }
 
 }
