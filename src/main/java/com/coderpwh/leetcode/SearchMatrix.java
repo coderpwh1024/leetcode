2de38d4feba3230508e8f6951007e82e00a6735c
coderpwh <pwhfbc@outlook.com>
2023-10-30 17:49:53 +0800

    fix:update SearchMatrix

--- src/main/java/com/coderpwh/leetcode/SearchMatrix.java
+++ src/main/java/com/coderpwh/leetcode/SearchMatrix.java
@@ -53,7 +53,6 @@ public class SearchMatrix {
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-
                 if (target == matrix[i][j]) {
                     return true;
                 }
