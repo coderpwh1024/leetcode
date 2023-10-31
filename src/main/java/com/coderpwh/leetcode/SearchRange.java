e539930235d22509717582f139c47183a5946126
coderpwh <pwhfbc@outlook.com>
2023-10-31 09:59:43 +0800

    feat:Init SearchRange

---
+++ src/main/java/com/coderpwh/leetcode/SearchRange.java
@@ -0,0 +1,50 @@
+package com.coderpwh.leetcode;
+
+/**
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ * <p>
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ * <p>
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * <p>
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ * nums 是一个非递减数组
+ * -109 <= target <= 109
+ *
+ * @author coderpwh
+ * @date 2023/10/31 9:57
+ */
+public class SearchRange {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+
+        return null;
+    }
+
+
+}
