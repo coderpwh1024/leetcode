8b4b41db6c7e4d13aadd32b1b5bda13d10b4a8a3
coder-PengWenHao <pengwenhao@aliyun.com>
2023-11-07 09:50:01 +0800

    feat:Init ConverInteger

---
+++ src/main/java/com/coderpwh/leetcode/ConvertInteger.java
@@ -0,0 +1,34 @@
+package com.coderpwh.leetcode;
+
+
+/***
+ *
+ * 整数转换。编写一个函数，确定需要改变几个位才能将整数A转成整数B。
+ *
+ * 示例1:
+ *
+ *  输入：A = 29 （或者0b11101）, B = 15（或者0b01111）
+ *  输出：2
+ * 示例2:
+ *
+ *  输入：A = 1，B = 2
+ *  输出：2
+ * 提示:
+ *
+ * A，B范围在[-2147483648, 2147483647]之间
+ *
+ *
+ */
+public class ConvertInteger {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int convertInteger(int A, int B) {
+
+        return  -1;
+    }
+
+
+}
