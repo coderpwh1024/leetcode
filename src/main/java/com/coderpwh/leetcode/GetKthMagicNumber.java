fd1c1b6e1995f3c3c69da844f3121e337047fccf
coderpwh <pengwenhao@aliyun.com>
2023-11-22 14:07:20 +0800

    feat:Init getKthMagicNumber

---
+++ src/main/java/com/coderpwh/leetcode/GetKthMagicNumber.java
@@ -0,0 +1,31 @@
+package com.coderpwh.leetcode;
+
+
+/**
+ * 面试题 17.09. 第 k 个数
+ * 提示
+ * 中等
+ * 253
+ * 相关企业
+ * 有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。注意，不是必须有这些素因子，而是必须不包含其他的素因子。例如，前几个数按顺序应该是 1，3，5，7，9，15，21。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: k = 5
+ * <p>
+ * 输出: 9
+ */
+public class GetKthMagicNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int getKthMagicNumber(int k) {
+
+        return -1;
+
+    }
+
+
+}
