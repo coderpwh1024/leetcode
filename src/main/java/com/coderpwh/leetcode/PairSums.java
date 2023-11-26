a32d20a98bc64b58a1b888b3bea56d5c322e9efb
coderpwh <pengwenhao@aliyun.com>
2023-11-26 14:33:46 +0800

    feat:Init PairSums

---
+++ src/main/java/com/coderpwh/leetcode/PairSums.java
@@ -0,0 +1,33 @@
+package com.coderpwh.leetcode;
+
+import java.util.List;
+/***
+ * 面试题 16.24. 数对和
+ * 提示
+ * 中等
+ * 50
+ * 相关企业
+ * 设计一个算法，找出数组中两数之和为指定值的所有整数对。一个数只能属于一个数对。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [5,6,5], target = 11
+ * 输出: [[5,6]]
+ * 示例 2:
+ *
+ * 输入: nums = [5,6,5,6], target = 11
+ * 输出: [[5,6],[5,6]]
+ * 提示：
+ */
+public class PairSums {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> pairSums(int[] nums, int target) {
+
+        return null;
+    }
+
+}
