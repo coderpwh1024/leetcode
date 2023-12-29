d3c459b5f3e9bf36dbf7957195c236ba6e7b1fec
pengwenhao <pengwenhao@qnvip.cn>
2023-12-29 16:42:31 +0800

    feat:init LocalDateTimeTest

---
+++ src/main/java/com/coderpwh/test/LocalDateTimeTest.java
@@ -0,0 +1,13 @@
+package com.coderpwh.test;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeTest {
+
+    public static void main(String[] args) {
+
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+
+    }
+}
