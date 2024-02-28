23891f4298baae0937ae5d7fb0d05e63455fe571
coderpwh <pengwenhao@aliyun.com>
2024-02-28 18:20:02 +0800

    update test code

---
+++ src/main/java/com/coderpwh/test/StringTest.java
@@ -0,0 +1,46 @@
+package com.coderpwh.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+
+/*        for(int i=0;i<10;i++){
+//            return new BaseResponseInfo(RespCodeEnum.R500.getCode(), "", "第" + i + 1 + "条数据的SPU不存在");
+            System.out.println("第"+i+1+"条数据的SPU不存在");
+        }*/
+
+
+
+    /*    String str = UUID.randomUUID().toString();
+        System.out.println(str);
+*/
+
+
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        StringBuffer buffer = new StringBuffer();
+        list.stream().forEach(r -> {
+            buffer.append(r).append(",");
+        });
+
+        System.out.println(buffer.toString());
+
+        String str = buffer.toString();
+
+        String[] arr = str.split(",");
+        System.out.println(arr.length);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
