98e12c099a151673d433961536b4efc2506f1c3c
coderpwh <pengwenhao@aliyun.com>
2024-04-04 16:02:02 +0800

    fix:update StringTest

--- src/main/java/com/coderpwh/view/StringTest.java
+++ src/main/java/com/coderpwh/view/StringTest.java
@@ -22,34 +22,45 @@ public class StringTest {
 
         System.out.println("---------------------------------");
 
-        String strArray ="ab&&2";
+        String strArray ="ab&&2&&c&&ef";
 
-        getStringArrayResult(strArray,str);
+
+        List<String> list =   getStringArrayResult(strArray,str);
+
+        for(String resultString:list){
+            System.out.println(resultString);
+        }
 
     }
 
 
 
-    public static String[] getStringArrayResult(String arr,String str){
+    public static List<String> getStringArrayResult(String arr,String str){
 
         if(arr.length()<=0){
             return  null;
         }
 
-        List<Integer> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
 
+        StringBuilder builder = new StringBuilder();
+        for(int i=0;i<arr.length();i++){
+            int j = arr.indexOf(str);
+            if(i==j){
+                if(builder.length()>0){
+                    list.add(builder.toString());
+                    builder.setLength(0);
+                }
+                i+=str.length();
+            }else{
+                builder.append(arr.charAt(i));
+            }
+        }
+        if(builder.length()>0){
+            list.add(builder.toString());
+        }
 
-         char[] ch = arr.toCharArray();
-
-         for(int i=0;i<ch.length;i++){
-
-         }
-
-         return  null;
-
-
-
-
+        return  list;
     }
 
 
