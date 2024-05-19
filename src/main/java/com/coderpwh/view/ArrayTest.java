2cb4d9bcaa356a4546814d49a4573b2a935b1709
coderpwh <pengwenhao@aliyun.com>
2024-05-19 22:47:17 +0800

    fix:update ArrayTest

--- src/main/java/com/coderpwh/view/ArrayTest.java
+++ src/main/java/com/coderpwh/view/ArrayTest.java
@@ -1,8 +1,28 @@
 package com.coderpwh.view;
 
+import java.util.List;
+
 public class ArrayTest {
     public static void main(String[] args) {
 
+        Integer[] arr = {2, 5, 4, 3, 1, 6, 8, 9};
+
+
+        ArrayTest arrayTest =  new ArrayTest();
+         List<Integer> result = arrayTest.exchange(arr);
+
+
+         for(int i=0;i<result.size();i++){
+             System.out.print(result.get(i));
+         }
+
+
+    }
+
+
+    public List<Integer> exchange(Integer[] arr) {
+
+        return null;
     }
 
 
