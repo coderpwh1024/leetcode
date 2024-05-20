4b4ffc8646eb31d9177038e645750431f7d360bd
coderpwh <pengwenhao@aliyun.com>
2024-05-20 13:55:43 +0800

    fix:update ArrayTest

--- src/main/java/com/coderpwh/view/ArrayTest.java
+++ src/main/java/com/coderpwh/view/ArrayTest.java
@@ -1,20 +1,26 @@
 package com.coderpwh.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
+/***
+ * 奇数在前，偶数在后，奇数的相对位置与偶数的相对位置保持不变
+ *
+ */
 public class ArrayTest {
     public static void main(String[] args) {
 
         Integer[] arr = {2, 5, 4, 3, 1, 6, 8, 9};
 
 
-        ArrayTest arrayTest =  new ArrayTest();
-         List<Integer> result = arrayTest.exchange(arr);
+        ArrayTest arrayTest = new ArrayTest();
+        List<Integer> result = arrayTest.exchange(arr);
 
 
-         for(int i=0;i<result.size();i++){
-             System.out.print(result.get(i));
-         }
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i)+" ");
+        }
 
 
     }
@@ -22,7 +28,22 @@ public class ArrayTest {
 
     public List<Integer> exchange(Integer[] arr) {
 
-        return null;
+
+        List<Integer> list = new ArrayList<>();
+        List<Integer> listOne = new ArrayList<>();
+        List<Integer> listTwo = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 == 0) {
+                listTwo.add(arr[i]);
+            } else {
+                listOne.add(arr[i]);
+            }
+        }
+
+        list.addAll(listOne);
+        list.addAll(listTwo);
+        return list;
     }
 
 
