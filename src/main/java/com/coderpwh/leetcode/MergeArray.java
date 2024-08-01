5c17d154e4a30a88d519356d21434d56317b6b7c
coderpwh <pengwenhao@aliyun.com>
2024-08-01 16:07:00 +0800

    fix:update MergeArray

--- src/main/java/com/coderpwh/leetcode/MergeArray.java
+++ src/main/java/com/coderpwh/leetcode/MergeArray.java
@@ -17,7 +17,9 @@ public class MergeArray {
         int nums2[] = {2, 5, 6};
         int n = 3;
 
-        mergeArray.merge(nums1, n, nums2, m);
+//        mergeArray.merge(nums1, n, nums2, m);
+
+        mergeArray.mergeTwo(nums1, n, nums2, m);
 
 
         for (int i = 0; i < nums1.length; i++) {
@@ -46,6 +48,41 @@ public class MergeArray {
     }
 
 
+    /***
+     *  双指针
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void mergeTwo(int[] nums1, int m, int[] nums2, int n) {
+
+        int p1 = 0;
+        int p2 = 0;
+        int cur;
+
+        int[] sorted = new int[m + n];
+
+        while (p1 < m || p2 < n) {
+
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1 + p2 - 1] = cur;
+        }
+
+
+        for (int i = 0; i != m + n; i++) {
+            nums1[i] = sorted[i];
+        }
+
+    }
 
 
 }
