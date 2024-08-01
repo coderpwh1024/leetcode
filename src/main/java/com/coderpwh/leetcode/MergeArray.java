8c2316c83b23438625454d61f4f1920fb75430bf
coderpwh <pengwenhao@aliyun.com>
2024-08-01 15:25:23 +0800

    feat:init MergeArray

---
+++ src/main/java/com/coderpwh/leetcode/MergeArray.java
@@ -0,0 +1,27 @@
+package com.coderpwh.leetcode;
+
+public class MergeArray {
+
+
+    public static void main(String[] args) {
+
+        MergeArray mergeArray = new MergeArray();
+
+        int nums1[] = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+
+        int nums2[] = {2, 5, 6};
+        int n = 3;
+
+
+        mergeArray.merge(nums1,n,nums2,m);
+
+
+    }
+
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+
+}
