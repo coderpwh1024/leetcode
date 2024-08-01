b528bf3f41840c4b00dda45a64159eda4afb5af9
coderpwh <pengwenhao@aliyun.com>
2024-08-01 15:22:19 +0800

    feat:init StringOne

---
+++ src/main/java/com/coderpwh/nk/StringOne.java
@@ -0,0 +1,25 @@
+package com.coderpwh.nk;
+
+import java.util.Scanner;
+
+public class StringOne {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner in  = new Scanner(System.in);
+
+        while (in.hasNext()){
+
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            System.out.println(a+b);
+        }
+
+
+    }
+
+
+}
