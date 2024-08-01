d338216afa7b964136215b5940f08dc95b189599
coderpwh <pengwenhao@aliyun.com>
2024-08-01 15:34:08 +0800

    fix:update MergeArray

--- src/main/java/com/coderpwh/leetcode/MergeArray.java
+++ src/main/java/com/coderpwh/leetcode/MergeArray.java
@@ -1,5 +1,9 @@
 package com.coderpwh.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MergeArray {
 
 
@@ -13,14 +17,23 @@ public class MergeArray {
         int nums2[] = {2, 5, 6};
         int n = 3;
 
+        mergeArray.merge(nums1, n, nums2, m);
 
-        mergeArray.merge(nums1,n,nums2,m);
+
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + " ");
+        }
 
 
     }
 
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        for (int i=0;i!=n;i++){
+            nums1[m+i]=nums2[i];
+        }
+        Arrays.sort(nums1);
 
     }
 
