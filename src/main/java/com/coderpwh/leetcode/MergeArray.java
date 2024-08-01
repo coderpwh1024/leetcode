e4d7764dffbf955bfffbed918158febcf670efd9
coderpwh <pengwenhao@aliyun.com>
2024-08-01 15:38:47 +0800

    fix:update MergeArray

--- src/main/java/com/coderpwh/leetcode/MergeArray.java
+++ src/main/java/com/coderpwh/leetcode/MergeArray.java
@@ -28,10 +28,18 @@ public class MergeArray {
     }
 
 
+    /***
+     * 常规方法
+     *  1. 时间复杂度为O(M+N)LOG(m+n)
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        for (int i=0;i!=n;i++){
-            nums1[m+i]=nums2[i];
+        for (int i = 0; i != n; i++) {
+            nums1[m + i] = nums2[i];
         }
         Arrays.sort(nums1);
 
