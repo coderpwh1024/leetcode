bd433351e66d8b4ef79f610719d8dc66ae7aa10d
coderpwh <pengwenhao@aliyun.com>
2024-08-05 14:02:48 +0800

    fix:update MajorityArray

--- src/main/java/com/coderpwh/leetcode/MajorityArray.java
+++ src/main/java/com/coderpwh/leetcode/MajorityArray.java
@@ -1,14 +1,20 @@
 package com.coderpwh.leetcode;
 
+import java.util.HashMap;
+
 public class MajorityArray {
 
     public static void main(String[] args) {
 
 
-        int nums[] = {2,2,1,1,1,2,2};
+//        int nums[] = {2, 2, 1, 1, 1, 2, 2};
+
+
+         int nums[] = {3,2,3};
+
 
         MajorityArray majorityArray = new MajorityArray();
-         int result = majorityArray.majorityElement(nums);
+        int result = majorityArray.majorityElement(nums);
 
         System.out.println(result);
 
@@ -17,5 +23,23 @@ public class MajorityArray {
 
     public int majorityElement(int[] nums) {
 
+        int flagCount = nums.length / 2;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+        for (Integer key : map.keySet()) {
+            if (map.get(key) > flagCount) {
+                return key;
+            }
+        }
+
+        return -1;
     }
 }
