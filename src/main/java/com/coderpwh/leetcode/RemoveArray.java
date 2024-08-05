4d5750bff6546d1eab78599f452a2d67d863d278
coderpwh <pengwenhao@aliyun.com>
2024-08-05 13:28:11 +0800

    feat:init RemoveArray

---
+++ src/main/java/com/coderpwh/leetcode/RemoveArray.java
@@ -0,0 +1,28 @@
+package com.coderpwh.leetcode;
+
+public class RemoveArray {
+
+
+    public static void main(String[] args) {
+
+
+        int nums[] = {};
+
+        int val = 0;
+
+
+        RemoveArray removeArray = new RemoveArray();
+        int result = removeArray.removeElement(nums, val);
+
+        System.out.println(result);
+
+    }
+
+
+    public int removeElement(int[] nums, int val) {
+
+
+        return  -1;
+    }
+
+}
