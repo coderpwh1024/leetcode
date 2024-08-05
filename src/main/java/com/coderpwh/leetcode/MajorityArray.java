641e6fd4c88e57525dc59783ba5a10bfa373af4e
coderpwh <pengwenhao@aliyun.com>
2024-08-05 13:55:44 +0800

    feat:init MajorityArray

---
+++ src/main/java/com/coderpwh/leetcode/MajorityArray.java
@@ -0,0 +1,21 @@
+package com.coderpwh.leetcode;
+
+public class MajorityArray {
+
+    public static void main(String[] args) {
+
+
+        int nums[] = {2,2,1,1,1,2,2};
+
+        MajorityArray majorityArray = new MajorityArray();
+         int result = majorityArray.majorityElement(nums);
+
+        System.out.println(result);
+
+    }
+
+
+    public int majorityElement(int[] nums) {
+
+    }
+}
