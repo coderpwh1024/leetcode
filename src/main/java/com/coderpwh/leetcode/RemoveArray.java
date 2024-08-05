f3de846250c7bcd38354a26804fb5e39d786cea2
coderpwh <pengwenhao@aliyun.com>
2024-08-05 13:52:50 +0800

    feat:init RemoveArray

--- src/main/java/com/coderpwh/leetcode/RemoveArray.java
+++ src/main/java/com/coderpwh/leetcode/RemoveArray.java
@@ -6,30 +6,38 @@ public class RemoveArray {
     public static void main(String[] args) {
 
 
-        int nums[] = {0, 1, 2, 2, 3, 0, 4, 2};
+//        int nums[] = {0, 1, 2, 2, 3, 0, 4, 2};
 
+//        int val = 2;
+
+
+        int nums[] = {3, 2, 2, 3};
         int val = 2;
 
 
         RemoveArray removeArray = new RemoveArray();
         int result = removeArray.removeElement(nums, val);
-
         System.out.println(result);
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
 
     }
 
 
     public int removeElement(int[] nums, int val) {
 
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
+        int n = nums.length;
+        int left = 0;
 
-            if(nums[i]==val){
-                count+=1;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] != val) {
+                nums[left] = nums[right];
+                left++;
             }
         }
-
-        return nums.length-count;
+        return left;
     }
 
 }
