3477f819cb999290e0a026d04c4eeeb6596acaf5
coderpwh <pengwenhao@aliyun.com>
2024-08-06 13:38:56 +0800

    fix:update

--- src/main/java/com/coderpwh/leetcode/RotateArray.java
+++ src/main/java/com/coderpwh/leetcode/RotateArray.java
@@ -31,13 +31,14 @@ public class RotateArray {
      */
     public void rotate(int[] nums, int k) {
 
+        int n = nums.length;
+        int[] newArr = new int[n];
 
-         int j =1;
-         int temp = nums[nums.length-1];
-         for(int i=nums.length-1;i>=1;){
-             nums[i]=nums[i--];
-         }
-         nums[0]=temp;
+        for (int i = 0; i < n; i++) {
+            newArr[(i + k) % n] = nums[i];
+        }
+
+        System.arraycopy(newArr, 0, nums, 0, n);
 
     }
 
