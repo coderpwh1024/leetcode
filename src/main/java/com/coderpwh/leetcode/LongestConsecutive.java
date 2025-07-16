72a91afe593ebee9f70297840a5989690e14e4f9
coderpwh <pengwenhao@aliyun.com>
2025-07-16 16:44:29 +0800

    feat:add LongstConsecutive

--- src/main/java/com/coderpwh/leetcode/LongestConsecutive.java
+++ src/main/java/com/coderpwh/leetcode/LongestConsecutive.java
@@ -1,12 +1,39 @@
 package com.coderpwh.leetcode;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
+/***
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ *
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ * 示例 2：
+ *
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+ * 输出：9
+ * 示例 3：
+ *
+ * 输入：nums = [1,0,1,2]
+ * 输出：3
+ *
+ *
+ */
 public class LongestConsecutive {
 
     public static void main(String[] args) {
-
-
         LongestConsecutive consecutive = new LongestConsecutive();
-        int[] nums = {100, 4, 200, 1, 3, 2};
+//        int[] nums = {100, 4, 200, 1, 3, 2};
+
+        int[] nums = {0,3,7,2,5,8,4,6,0,1};
 
         int result = consecutive.longestConsecutive(nums);
         System.out.println(result);
@@ -21,16 +48,25 @@ public class LongestConsecutive {
      * @return
      */
     public int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+        int longestStreak = 0;
 
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[j]==nums[i]+1){
-                    nums[j]=nums[i];
+        for (int num : num_set) {
+            if(!num_set.contains(num-1)){
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum+1)){
+                    currentNum++;
+                    currentStreak++;
                 }
+                longestStreak = Math.max(longestStreak,currentStreak);
             }
         }
-        return nums.length;
+        return  longestStreak;
     }
-
 
 }
